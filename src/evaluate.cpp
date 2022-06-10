32026558715afd119cb24fdbbe4339384a830e67
FauziAkram <fauzi.dabat@hotmail.com>
2022-06-10 13:50:49 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1077,14 +1077,14 @@ Value Eval::evaluate(const Position& pos) {
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
   bool useClassical = (pos.this_thread()->depth > 9 || pos.count<ALL_PIECES>() > 7) &&
-          abs(eg_value(pos.psq_score())) * 6 > (858 + pos.non_pawn_material() / 64) * (11 + pos.rule50_count());
+          abs(eg_value(pos.psq_score())) * 6 > (860 + pos.non_pawn_material() / 64) * (11 + pos.rule50_count());
 
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
   if (!useNNUE || useClassical)
   {
       v = Evaluation<NO_TRACE>(pos).value();          // classical
-      useClassical = abs(v) >= 294;
+      useClassical = abs(v) >= 291;
   }
 
   // If result of a classical evaluation is much lower than threshold fall back to NNUE
@@ -1097,16 +1097,16 @@ Value Eval::evaluate(const Position& pos) {
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
        Value nnue     = NNUE::evaluate(pos, true, &complexity);     // NNUE
 
-       complexity = (137 * complexity + 136 * abs(nnue - psq)) / 256;
+       complexity = (138 * complexity + 136 * abs(nnue - psq)) / 256;
        optimism = optimism * (256 + complexity) / 256;
-       v = (nnue * scale + optimism * (scale - 838)) / 1024;
+       v = (nnue * scale + optimism * (scale - 821)) / 1024;
 
        if (pos.is_chess960())
            v += fix_FRC(pos);
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (194 - pos.rule50_count()) / 214;
+  v = v * (194 - pos.rule50_count()) / 217;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
