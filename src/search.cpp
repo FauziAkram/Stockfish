a2b5138b68fd9eae0879babe6623a027fc3f8777
FauziAkram <fauzi.dabat@hotmail.com>
2022-06-12 21:15:31 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1035,22 +1035,37 @@ moves_loop: // When in check, search starts here
                             + (*contHist[1])[movedPiece][to_sq(move)]
                             + (*contHist[3])[movedPiece][to_sq(move)];
 
-              // Continuation history based pruning (~2 Elo)
-              if (   lmrDepth < 5
-                  && history < -3875 * (depth - 1))
-                  continue;
-
-              history += thisThread->mainHistory[us][from_to(move)];
-
               // Futility pruning: parent node (~9 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 11
-                  && ss->staticEval + 122 + 138 * lmrDepth + history / 60 <= alpha)
+                  && ss->staticEval + 124 + 140 * lmrDepth + (history + thisThread->mainHistory[us][from_to(move)]) / 60 <= alpha)
                   continue;
 
               // Prune moves with negative SEE (~3 Elo)
               if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth)))
                   continue;
+              
+              // Continuation history based pruning (~2 Elo)
+              if (   lmrDepth < 5
+                  && history < - 3890 * (depth - 1))
+              {
+                  if (lmrDepth < 2)
+                      continue;
+
+                  ss->doubleExtensions = (ss-1)->doubleExtensions;
+                  ss->currentMove = move;
+                  ss->continuationHistory = &thisThread->continuationHistory[ss->inCheck]
+                                                                            [capture]
+                                                                            [movedPiece]
+                                                                            [to_sq(move)];
+
+                  pos.do_move(move, st, givesCheck);
+                  value = -qsearch<NonPV>(pos, ss+1, -alpha-1, -alpha);
+                  pos.undo_move(move);
+
+                  if (value <= alpha)
+                      continue;
+              }
           }
       }
 
