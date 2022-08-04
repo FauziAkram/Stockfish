dfc0f5541e4d9533b1117c5eddb4e20808bd7c47
FauziAkram <fauzi.dabat@hotmail.com>
2022-08-04 14:59:43 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((9 * d + 270) * d - 311 , 2145);
+    return std::min((10 * d + 278) * d - 305 , 1897);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
