6d76d27cc520b32dce325c71772662d735f7ac8a
FauziAkram <fauzi.dabat@hotmail.com>
2022-08-08 18:34:15 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -802,10 +802,10 @@ namespace {
     // Step 9. Null move search with verification search (~22 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 14695
+        && (ss-1)->statScore < 14736
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 15 * depth - improvement / 15 + 201 + complexity / 24
+        &&  ss->staticEval >= beta - 17 * depth - improvement / 14 + 224 + complexity / 26
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
