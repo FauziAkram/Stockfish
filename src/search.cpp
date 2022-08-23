3dbda60bb15561fa19fa2a9724a91ae2db0f3d6f
FauziAkram <fauzi.dabat@hotmail.com>
2022-08-23 13:57:23 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -158,7 +158,7 @@ namespace {
 void Search::init() {
 
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int((20.81 + std::log(Threads.size()) / 2) * std::log(i));
+      Reductions[i] = int((16.19 + std::log(Threads.size()) / 2) * std::log(i));
 }
 
 
