1cddf6fd4593438e436c4b61f900f05993118c7b
FauziAkram <fauzi.dabat@hotmail.com>
2022-09-30 03:05:49 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,6 +191,13 @@ namespace Trace {
 using namespace Trace;
 
 namespace {
+  
+  int faz3=1064; TUNE(SetRange(950,1130),faz3);
+  int faz4=106, faz1=100, faz2=100; TUNE(SetRange(30, 200), faz1, faz2, faz4);
+  int faz5=104, faz6=131; TUNE(SetRange(50, 200), faz5, faz6);
+  int faz7=269; TUNE(SetRange(160, 400), faz7);
+  int faz8=754; TUNE(SetRange(630, 870), faz8);
+  int faz9=195, faz10=211; TUNE(SetRange(150, 260), faz9, faz10);
 
   // Threshold for lazy and space evaluation
   constexpr Value LazyThreshold1    =  Value(3631);
@@ -1059,25 +1066,29 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   // PSQ advantage is decisive and several pieces remain (~3 Elo)
   bool useClassical = !useNNUE || (pos.count<ALL_PIECES>() > 7 && abs(psq) > 1760);
   if (useClassical)
-      v = Evaluation<NO_TRACE>(pos).value();
+      {
+    v = Evaluation<NO_TRACE>(pos).value();
+    // Damp down Classical evaluation linearly with 50 move rule
+    v = v * (faz1 - pos.rule50_count()) / faz2;
+  }
   else
   {
       int nnueComplexity;
-      int scale = 1064 + 106 * pos.non_pawn_material() / 5120;
+      int scale = faz3 + faz4 * pos.non_pawn_material() / 5120;
       Value optimism = pos.this_thread()->optimism[stm];
 
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (104 * nnueComplexity + 131 * abs(nnue - psq)) / 256;
+      nnueComplexity = (faz5 * nnueComplexity + faz6 * abs(nnue - psq)) / 256;
       if (complexity) // Return hybrid NNUE complexity to caller
           *complexity = nnueComplexity;
 
-      optimism = optimism * (269 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 754)) / 1024;
-  }
+      optimism = optimism * (faz7 + nnueComplexity) / 256;
+      v = (nnue * scale + optimism * (scale - faz8)) / 1024;
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (195 - pos.rule50_count()) / 211;
+  v = v * (faz9 - pos.rule50_count()) / faz10;
+  }
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
