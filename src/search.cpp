d0f6a5f9a49e9bb80f82485925b1081877f84df9
FauziAkram <fauzi.dabat@hotmail.com>
2022-12-18 22:58:06 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1098,6 +1098,7 @@ moves_loop: // When in check, search starts here
 
           // Quiet ttMove extensions (~0 Elo)
           else if (   PvNode
+                   && false
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5177)
