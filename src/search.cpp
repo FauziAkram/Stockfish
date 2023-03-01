a4842939108b2117c59293676f6feb4fe14df4a5
FauziAkram <fauzi.dabat@hotmail.com>
2023-03-01 16:56:09 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1178,8 +1178,8 @@ moves_loop: // When in check, search starts here
           r++;
 
       // Decrease reduction if move is a killer and we have a good history
-      if (move == ss->killers[0]
-          && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
+      if (   (move == ss->killers[0] || move == ss->killers[1])
+          && (*contHist[0])[movedPiece][to_sq(move)] >= 3720)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
