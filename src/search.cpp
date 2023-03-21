17155d8fa3d2281f93d3643156fdb477e1c11090
FauziAkram <fauzi.dabat@hotmail.com>
2023-03-21 18:13:58 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1102,8 +1102,8 @@ moves_loop: // When in check, search starts here
                   return singularBeta;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
-              else if (ttValue >= beta)
-                  extension = -2 - !PvNode;
+              //else if (ttValue >= beta)
+              //    extension = -2 - !PvNode;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
