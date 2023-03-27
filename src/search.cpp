58452c2a411ec574456087e3b16a8d3f59614891
FauziAkram <fauzi.dabat@hotmail.com>
2023-03-27 14:56:17 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1228,7 +1228,7 @@ moves_loop: // When in check, search starts here
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (alpha + 57 + 12 * (newDepth - d));
+              const bool doDeeperSearch = value > (alpha + 57 + 13 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 580 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
