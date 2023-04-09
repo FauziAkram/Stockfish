4d42d500cfe3d76d4aaa317dc7e71e7f9cbb76a5
FauziAkram <fauzi.dabat@hotmail.com>
2023-04-10 00:00:35 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -58,6 +58,14 @@ using Eval::evaluate;
 using namespace Search;
 
 namespace {
+  int xx1=50, xx2=2, xx3=12, xx4=588, xx5=5, xx6=87;
+  TUNE(xx1);
+  TUNE(SetRange(-1, 10), xx2);
+  TUNE(SetRange(0, 30), xx3);
+  TUNE(xx4);
+  TUNE(SetRange(0, 15), xx5);
+  TUNE(xx6);
+
 
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV, Root };
@@ -1222,8 +1230,8 @@ moves_loop: // When in check, search starts here
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (alpha + 58 + 12 * (newDepth - d));
-              const bool doEvenDeeperSearch = value > alpha + 588 && ss->doubleExtensions <= 5;
+              const bool doDeeperSearch = value > (alpha + xx1 + xx2 * depth + xx3 * (newDepth - d));
+              const bool doEvenDeeperSearch = value > alpha + xx4 && ss->doubleExtensions <= xx5;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
@@ -1338,7 +1346,7 @@ moves_loop: // When in check, search starts here
 
                   // Reduce other moves if we have found at least one score improvement (~1 Elo)
                   if (   depth > 1
-                      && ((improving && complexity > 971) || (value < (5 * alpha + 75 * beta) / 87) || depth < 6)
+                      && ((improving && complexity > 971) || (value < (5 * alpha + 75 * beta) / xx6) || depth < 6)
                       && beta  <  12535
                       && value > -12535) {
                       bool extraReduction = depth > 2 && alpha > -12535 && bestValue != -VALUE_INFINITE && (value - bestValue) > (7 * (beta - alpha)) / 8;
