8dbfac3ae14b95849a9e1774e1473875c520bc27
FauziAkram <fauzi.dabat@hotmail.com>
2023-06-19 02:11:29 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1142,7 +1142,7 @@ moves_loop: // When in check, search starts here
       // Decrease further on cutNodes. (~1 Elo)
       if (   ss->ttPv
           && !likelyFailLow)
-          r -= !PvNode && tte->depth() >= depth + 3 ? 3 : 2;
+          r -= !PvNode && tte->depth() >= depth + 3 ? 4 : 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 8)
