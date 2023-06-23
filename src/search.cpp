9981533e1fa781e13f0fca4c9044905c5d6b2279
FauziAkram <fauzi.dabat@hotmail.com>
2023-06-23 14:18:34 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1063,7 +1063,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (82 + 65 * (ss->ttPv && !PvNode)) * depth / 64;
+              Value singularBeta = ttValue - (84 + 63 * (ss->ttPv && !PvNode)) * depth / 64;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
@@ -1077,12 +1077,14 @@ moves_loop: // When in check, search starts here
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
-                      && value < singularBeta - 21
+                      && value < singularBeta - 22
                       && ss->doubleExtensions <= 11)
                   {
                       extension = 2;
                       depth += depth < 13;
                   }
+                  if (value >= singularBeta - singularDepth)
+                       depth--;
               }
 
               // Multi-cut pruning
