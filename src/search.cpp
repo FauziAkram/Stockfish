ce3e228c36c8d16dd98c7cc0e676f5c6280165ff
FauziAkram <fauzi.dabat@hotmail.com>
2023-06-24 19:47:52 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -976,8 +976,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = newDepth - r - (   type_of(move) == PROMOTION
-                                                   && move != countermove
+          int lmrDepth = newDepth - r - (             move != countermove
                                                    && move != ss->killers[0]
                                                    && move != ss->killers[1]);
 
