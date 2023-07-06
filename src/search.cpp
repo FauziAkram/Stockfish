401be47b2b94b3e12ec26a5d9e74c9fc5d0a8854
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-06 21:28:46 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop: // When in check, search starts here
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 9)
-              extension = (pos.count<ALL_PIECES>() <= 12 && (pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 6)) && pos.see_ge(move) ? 2 : 1;
+              extension = (pos.count<ALL_PIECES>() <= 12 && (pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 6)) && pos.see_ge(move);
 
           // Quiet ttMove extensions (~1 Elo)
           else if (   PvNode
