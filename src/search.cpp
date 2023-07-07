71ecf8223d9b1a3e19ac2f328622b6be90db466f
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-07 22:51:20 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
-          if (rootNode && (capture || move != ttMove && extension <= 0))
+          if (rootNode && (capture || (move != ttMove && extension <= 0)))
           newDepth++;
 
           value = -search<PV>(pos, ss+1, -beta, -alpha, newDepth, false);
