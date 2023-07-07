71da024c9498de2d4a4142c2f69a222707389df6
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-07 16:10:05 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 3;
+              delta += delta / 3 + 1 + failedHighCnt;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
