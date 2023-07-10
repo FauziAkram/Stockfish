17340e7e6406e843cc56591d08f2fe404a2247c1
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-11 01:20:57 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1024,7 +1024,7 @@ moves_loop: // When in check, search starts here
 
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 6
-                  && history < (-xx1 * depth)+(xx2 * alpha))
+                  && history < (-xx1 * depth)+(xx2 * singularBeta))
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
