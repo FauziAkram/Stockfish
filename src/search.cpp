21df5c89fcbf700e35f1573fbf3b789e1e3cb054
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-15 00:20:22 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1755,7 +1755,7 @@ moves_loop: // When in check, search starts here
         if (ss->inCheck && i > 2)
             break;
         if (is_ok((ss-i)->currentMove))
-            (*(ss-i)->continuationHistory)[pc][to] << bonus;
+            *(ss-i)->continuationHistory[pc][to] << bonus;
     }
   }
 
