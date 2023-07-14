fe85ab05977befb9a16269f3b53bc7e86ec0a9cb
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-15 01:06:04 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1767,8 +1767,8 @@ moves_loop: // When in check, search starts here
     // Update killers
     if (ss->killers[0] != move)
     {
-        ss->killers[1] = ss->killers[0];
-        ss->killers[0] = move;
+        ss->killers[0] = ss->killers[1];
+        ss->killers[1] = move;
     }
 
     Color us = pos.side_to_move();
