2b868cc684ee606e1cd2798c1bc25105d8366725
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-16 03:20:48 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -777,8 +777,7 @@ namespace {
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 17329
-        &&  eval >= beta
-        &&  eval >= ss->staticEval
+        &&  eval >= ss->staticEval - 15 * depth
         &&  ss->staticEval >= beta - 21 * depth + 258
         && !excludedMove
         &&  pos.non_pawn_material(us)
