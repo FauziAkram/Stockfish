cadf598969f5d66f52bc7d5fdf8dc022f92d989e
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-19 02:28:05 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1121,6 +1121,10 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5168)
               extension = 1;
+         if (move != ttMove && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
+         {
+              extension = 1;
+         }
       }
 
       // Add extension to new depth
