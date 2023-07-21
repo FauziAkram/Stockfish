a8e965ce9c515a5275ae9b70bdc0fb8a186b90fb
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-22 00:58:40 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1020,12 +1020,12 @@ moves_loop: // When in check, search starts here
 
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 6
-                  && history < -3832 * depth)
+                  && history < ((-5555 * depth) + (8 * beta)))
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
-              lmrDepth += history / 7011;
+              lmrDepth += history / 6953;
               lmrDepth = std::max(lmrDepth, -2);
 
               // Futility pruning: parent node (~13 Elo)
