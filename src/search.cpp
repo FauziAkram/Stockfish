011081e73f7dc8816327c007a9faca61afec3095
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-23 18:57:42 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1015,7 +1015,7 @@ moves_loop: // When in check, search starts here
           else
           {
               int history =   (*contHist[0])[movedPiece][to_sq(move)] * 2
-                            + (*contHist[1])[movedPiece][to_sq(move)] * 2
+                            + (*contHist[1])[movedPiece][to_sq(move)]
                             + (*contHist[3])[movedPiece][to_sq(move)];
 
               // Continuation history based pruning (~2 Elo)
@@ -1025,7 +1025,7 @@ moves_loop: // When in check, search starts here
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
-              lmrDepth += history / 8660;
+              lmrDepth += history / 8620;
               lmrDepth = std::max(lmrDepth, -2);
 
               // Futility pruning: parent node (~13 Elo)
