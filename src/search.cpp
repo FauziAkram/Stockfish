740dcf2a261ac5165952ddcce0821b383d39762e
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-27 02:22:35 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1480,6 +1480,10 @@ moves_loop: // When in check, search starts here
             // In case of null move search use previous static eval with a different sign
             ss->staticEval = bestValue = (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
                                                                           : -(ss-1)->staticEval;
+      
+        // Update the main history for null moves
+        if ((ss-1)->currentMove == MOVE_NULL)
+        thisThread->mainHistory.update(pos, VALUE_NONE, depth, true);
 
         // Stand pat. Return immediately if static value is at least beta
         if (bestValue >= beta)
