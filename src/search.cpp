4bb4cf8854ea3951fa350b95c4aceb343fe6fd36
FauziAkram <fauzi.dabat@hotmail.com>
2023-07-26 22:35:07 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -983,7 +983,7 @@ moves_loop: // When in check, search starts here
           {
               // Futility pruning for captures (~2 Elo)
               if (   !givesCheck
-                  && lmrDepth < 7
+                  && lmrDepth < 5 + ss->ply / 3
                   && !ss->inCheck
                   && ss->staticEval + 197 + 248 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 7 < alpha)
