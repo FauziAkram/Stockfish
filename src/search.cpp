f959033a6e22c96fc9bd708a5f9d2a128155dd93
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-06 16:54:02 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1569,7 +1569,7 @@ moves_loop: // When in check, search starts here
                 continue;
 
             // Do not search moves with bad enough SEE values (~5 Elo)
-            if (!pos.see_ge(move, Value(-97 + (givesCheck ? 12 : 0))))
+            if (!pos.see_ge(move, Value(-97 + (8 * givesCheck))))
                 continue;
         }
 
