3eed309bc95e00e5049c04fda0877b9434c20068
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-07 13:20:26 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -144,20 +144,20 @@ void MovePicker::score() {
 
           // bonus for escaping from capture
           m.value += threatenedPieces & from ?
-                       (pt == QUEEN && !(to & threatenedByRook)  ? 50000
-                      : pt == ROOK  && !(to & threatenedByMinor) ? 25000
-                      :                !(to & threatenedByPawn)  ? 15000
+                       (pt == QUEEN && !(to & threatenedByRook)  ? 49982
+                      : pt == ROOK  && !(to & threatenedByMinor) ? 26113
+                      :                !(to & threatenedByPawn)  ? 16055
                       :                                            0 )
                       :                                            0 ;
 
           // malus for putting piece en prise
           m.value -= !(threatenedPieces & from) ?
-                        (pt == QUEEN ?   bool(to & threatenedByRook)  * 50000
-                                       + bool(to & threatenedByMinor) * 10000
-                                       + bool(to & threatenedByPawn)  * 20000
-                       : pt == ROOK  ?   bool(to & threatenedByMinor) * 25000
-                                       + bool(to & threatenedByPawn)  * 10000
-                       : pt != PAWN ?    bool(to & threatenedByPawn)  * 15000
+                        (pt == QUEEN ?   bool(to & threatenedByRook)  * 50528
+                                       + bool(to & threatenedByMinor) * 10003
+                                       + bool(to & threatenedByPawn)  * 19038
+                       : pt == ROOK  ?   bool(to & threatenedByMinor) * 25286
+                                       + bool(to & threatenedByPawn)  * 9619
+                       : pt != PAWN ?    bool(to & threatenedByPawn)  * 14748
                        :                                                0 )
                        :                                                0 ;
       }
@@ -252,7 +252,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -3036 * depth);
       }
 
       ++stage;
