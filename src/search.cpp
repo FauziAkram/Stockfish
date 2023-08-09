3c3d619d8ad250511ce4a22a1d76619aa7b149d1
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-09 17:11:02 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1157,7 +1157,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
-          r -= r -= (depth > 1) + (depth < 4 || depth > 22);
+          r -= r -= (depth > 1) + (depth < 4 || depth > 23);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
