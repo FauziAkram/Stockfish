ffd2a618d411bf23a1487442b17824f55b063b22
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-10 23:37:05 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -70,9 +70,9 @@ namespace {
   // Reductions lookup table initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
-  Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
+  Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta, bool ttHit) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 1372 - int(delta) * 1073 / int(rootDelta)) / 1024 + (!i && r > 936);
+    return (r + 1370 - (10 * ttHit) - int(delta) * 1111 / int(rootDelta)) / 1024 + (!i && r > 914);
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
@@ -965,7 +965,7 @@ moves_loop: // When in check, search starts here
 
       Value delta = beta - alpha;
 
-      Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
+      Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta, ss->ttHit);
 
       // Step 14. Pruning at shallow depth (~120 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
