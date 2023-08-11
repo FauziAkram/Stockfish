0dfd7080c01c7510d4ae1cfc7179fb6341fe6ee9
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-11 22:45:18 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1024,7 +1024,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               if (   ttCapture
-              && move == (ss->killers[1] || ss->killers[0])
+              && move == ss->killers[1]
               && (ss-1)->ttPv
               && likelyFailLow){
               dbg_mean_of(1);
