d39ec1e6f1c09deec26abfe5e4fe92f3e5671e0d
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-13 15:19:48 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1000,7 +1000,7 @@ moves_loop: // When in check, search starts here
                             + (*contHist[3])[movedPiece][to_sq(move)];
 
               // Continuation history based pruning (~2 Elo)
-              if (   lmrDepth < 6 + to_sq(move) == to_sq((ss-2)->killers[1])
+              if (   lmrDepth < 6 + (to_sq(move) == to_sq((ss-2)->killers[1]))
                   && history < -3832 * depth)
                   continue;
 
