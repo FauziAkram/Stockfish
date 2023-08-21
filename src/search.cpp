e9d41832d6ba3ad1e42797d279bfb18ce9e5ae64
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-22 02:54:18 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1151,6 +1151,9 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      if (pos.non_pawn_material() < 3500)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
