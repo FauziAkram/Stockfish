c96c285dd9ef7ddb5e56772188ac05aed39b6e01
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-22 03:37:43 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -72,7 +72,7 @@ namespace {
 
   Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 1372 - int(delta) * 1073 / int(rootDelta) - d / 32) / 1024 + (!i && r > 936);
+    return (r + 1372 - int(delta) * 1073 / int(rootDelta) - d) / 1024 + (!i && r > 936);
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
