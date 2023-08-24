0ae3c60468e85f4edcec79333fcc4c0f5a845ef0
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-25 02:40:04 +0300

    Update position.h

--- src/position.h
+++ src/position.h
@@ -310,8 +310,8 @@ inline Key Position::key() const {
 template<bool AfterMove>
 inline Key Position::adjust_key50(Key k) const
 {
-  return st->rule50 < 14 - AfterMove
-      ? k : k ^ make_key((st->rule50 - (14 - AfterMove)) / 8);
+  return st->rule50 < 13 - AfterMove
+      ? k : k ^ make_key((st->rule50 - (13 - AfterMove)) / 8);
 }
 
 inline Key Position::material_key() const {
