0c6bdeaad1fd8badf155627cbb4a3417fb16da21
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-26 04:11:31 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -71,7 +71,12 @@ namespace {
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
   Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
+    bool lmp = mn > d / 2 + 2 && d >= 3;
     int r = Reductions[d] * Reductions[mn];
+    if (lmp)
+    {
+    reductionScale += reductionScale / 2;
+    }
     return (r + 1372 - int(delta) * 1073 / int(rootDelta)) / 1024 + (!i && r > 936);
   }
 
