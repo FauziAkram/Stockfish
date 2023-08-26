2cde313d9bbaac434202bec9c55f75550ec8ddb1
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-26 03:27:43 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1696,7 +1696,7 @@ moves_loop: // When in check, search starts here
     Piece moved_piece = pos.moved_piece(bestMove);
     PieceType captured;
 
-    int quietMoveBonus = stat_bonus(depth + 1);
+    int quietMoveBonus = stat_bonus(depth + 1) * std::max(2, depth/5);
 
     if (!pos.capture_stage(bestMove))
     {
