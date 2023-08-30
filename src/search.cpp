46443e24df415fff842917a9cd98e5cbe0324900
FauziAkram <fauzi.dabat@hotmail.com>
2023-08-30 17:15:15 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -729,8 +729,9 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (    ttValue != VALUE_NONE
-            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
-            std::abs(ttValue - eval) <= xx1)
+            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER))
+            && std::abs(ttValue - eval) <= xx1)
+            
     {
         eval = ttValue;
     }
