fa9f98cdc5dca06b4938121c69eae110cda9c856
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-04 01:08:17 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ namespace {
         && (ss-1)->statScore < 17329
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 21 * depth + 258
+        &&  ss->staticEval >= beta - 21 * depth + 258 - 20 * cutNode - 20 * !ss->ttHit
         && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  ss->ply >= thisThread->nmpMinPly
