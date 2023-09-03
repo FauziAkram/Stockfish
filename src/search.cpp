62fad3d351edc9662301c5f14da1a6f3aa554b2f
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-04 00:08:29 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -46,8 +46,8 @@
 #include "uci.h"
 
 namespace Stockfish {
-int xx1= 200, xx2=0, xx3=0;
-TUNE(SetRange(-350, 400), xx1,xx2,xx3);
+int xx1= 200, xx2=0, xx3=0, xx4=0, xx5=0;
+TUNE(SetRange(-350, 400), xx1,xx2,xx3, xx4, xx5);
 
 
 namespace Search {
@@ -1550,7 +1550,7 @@ moves_loop: // When in check, search starts here
                 if (moveCount > 2)
                     continue;
 
-                futilityValue = futilityBase + xx2 * !capture + PieceValue[pos.piece_on(to_sq(move))];
+                futilityValue = futilityBase + xx2 * !capture + xx3 * PvNode + PieceValue[pos.piece_on(to_sq(move))];
 
                 if (futilityValue <= alpha)
                 {
@@ -1558,7 +1558,7 @@ moves_loop: // When in check, search starts here
                     continue;
                 }
 
-                if (futilityBase + xx3 * !capture <= alpha && !pos.see_ge(move, VALUE_ZERO + 1))
+                if (futilityBase + xx3 * !capture + xx4 * PvNode <= alpha && !pos.see_ge(move, VALUE_ZERO + 1))
                 {
                     bestValue = std::max(bestValue, futilityBase);
                     continue;
