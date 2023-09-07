d8230cdff4703cd738e0efc059304c203318072c
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-07 13:58:40 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1541,7 +1541,7 @@ moves_loop: // When in check, search starts here
             // Futility pruning and moveCount pruning (~10 Elo)
             if (   !givesCheck
                 &&  to_sq(move) != prevSq
-                &&  futilityBase > -VALUE_KNOWN_WIN
+                &&  futilityBase - (tte->bound() & BOUND_LOWER) > -VALUE_KNOWN_WIN
                 &&  type_of(move) != PROMOTION)
             {
                 if (moveCount > 2)
