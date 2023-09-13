e80a298df57ad68128dc3ab7cf0ad82d37b65e98
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-13 20:55:05 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -141,8 +141,6 @@ void MovePicker::score() {
           m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)];
           m.value += 2 * (*continuationHistory[0])[pc][to];
           m.value +=     (*continuationHistory[1])[pc][to];
-          m.value +=     (*continuationHistory[3])[pc][to];
-          m.value +=     (*continuationHistory[5])[pc][to];
 
           // bonus for checks
           m.value += bool(pos.check_squares(pt) & to) * 16384;
