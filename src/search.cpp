82bacd86f28d770ef7cf89ebe92c72b3a5ea904e
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-19 21:14:47 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -73,7 +73,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
-    return Value((140 - 40 * noTtCutNode) * (d - improving));
+    return Value((140 - 40 * noTtCutNode) * (d - improving * 1.2));
   }
 
   // Reductions lookup table initialized at startup
