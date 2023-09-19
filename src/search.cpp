26b427ea89bd92f46c89d02abf409edee948ed15
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-19 20:24:55 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -843,7 +843,7 @@ namespace {
     if (    cutNode
         &&  depth >= 8
         && !ttMove)
-        depth -= 2;
+        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth + 3);
 
     probCutBeta = beta + 168 - 61 * improving;
 
