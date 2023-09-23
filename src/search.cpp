0cfbc49d8b3a0a9d612a3ea5bf5a9d73c64d39ce
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-23 13:44:23 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1208,9 +1208,11 @@ moves_loop: // When in check, search starts here
               if (newDepth > d)
                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
 
-              int bonus = value <= alpha ? -stat_bonus(newDepth)
-                        : value >= beta  ?  stat_bonus(newDepth)
-                                         :  0;
+              int bonus = value <= alpha + 346 ? -stat_bonus(newDepth) * 0.5
+                        : value <= alpha       ? -stat_bonus(newDepth)
+                        : value >= beta + 431  ?  stat_bonus(newDepth)
+                        : value >= beta        ?  stat_bonus(newDepth) * 1.1
+                                               :  0;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
