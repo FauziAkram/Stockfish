9861896ea3a31f2a755563c19869403c2f9b7274
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-27 02:36:55 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,7 +176,7 @@ Value Eval::evaluate(const Position& pos) {
       nnue     -= nnue     * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
 
       int npm = pos.non_pawn_material() / 64;
-      v = (  nnue     * (920 + npm + 9 * pos.count<PAWN>())
+      v = (  nnue     * (924 + npm + 9 * pos.count<PAWN>())
            + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
   }
 
