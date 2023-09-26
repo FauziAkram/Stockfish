2e3f59cbf77d83a907d35cbe8c04ae76318f5448
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-26 19:02:30 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -995,8 +995,8 @@ moves_loop: // When in check, search starts here
               if (   !givesCheck
                   && lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 258 * lmrDepth + PieceValue[pos.piece_on(to_sq(move))]
-                   + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 6 < alpha)
+                  && ss->staticEval + 260 * lmrDepth + PieceValue[pos.piece_on(to_sq(move))]
+                   + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 7 < alpha)
                   continue;
 
               // SEE based pruning for captures and checks (~11 Elo)
