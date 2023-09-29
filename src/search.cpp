c797f684d1efcd14f0649dcc652447447d36e06b
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-30 00:31:03 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes (~2 Elo)
       if (PvNode)
-          r -= 1 + (move == ss->killers[0] || move == ss->killers[1]);
+          r -= 1 + (move == ss->killers[0]);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
