a94fca0080239bf991dad6930919cde7b3f04a14
FauziAkram <fauzi.dabat@hotmail.com>
2023-09-30 13:48:17 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1029,7 +1029,7 @@ moves_loop: // When in check, search starts here
               lmrDepth = std::max(lmrDepth, 0);
 
               // Prune moves with negative SEE (~4 Elo)
-              if (!pos.see_ge(move, Value(-31 * lmrDepth * lmrDepth)))
+              if (!capture && !pos.see_ge(move, Value(-31 * lmrDepth * lmrDepth)))
                   continue;
           }
       }
