78b8d433dde3bfe264c0e7f64728bc6e9ae45ee5
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-01 13:05:25 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1001,7 +1001,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning for captures and checks (~11 Elo)
-              if (!pos.see_ge(move, Value(-205) * depth))
+              if (!pos.see_ge(move, Value(-205) * (depth - 1)))
                   continue;
           }
           else
