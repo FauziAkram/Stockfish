f405341814a09b2f40be217eccef7927c29c0e93
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-04 01:21:50 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1017,7 +1017,7 @@ moves_loop: // When in check, search starts here
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
-              lmrDepth += history / 7011;
+              lmrDepth += history / 7000;
               lmrDepth = std::max(lmrDepth, -2);
 
               // Futility pruning: parent node (~13 Elo)
@@ -1209,7 +1209,7 @@ moves_loop: // When in check, search starts here
               if (newDepth > d)
                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
 
-              int bonus = value <= alpha       ? -stat_bonus(newDepth)
+              int bonus = value <= alpha       ? -stat_bonus(newDepth) * 0.9
                         : value >= beta + 431  ?  stat_bonus(newDepth)
                         : value >= beta        ?  stat_bonus(newDepth) * 1.1
                                                :  0;
