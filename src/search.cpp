0e7a31a00a1db90a86f33bb3b5fb147619d2e4bf
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-04 16:13:54 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -365,7 +365,7 @@ void Thread::search() {
 
           // Adjust optimism based on root move's previousScore (~4 Elo)
           int clampedPrev = std::clamp(int(prev), -6150, 6200);
-          int opt = 103 * clampedPrev / (std::abs(clampedPrev) + 149);
+          int opt = 103 * clampedPrev / (std::abs(prev) + 149);
           optimism[ us] = Value(opt);
           optimism[~us] = -optimism[us];
 
