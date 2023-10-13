ba8ca744b78373db632fce5501a723c87975e326
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-13 16:02:10 +0300

    Update thread.h

--- src/thread.h
+++ src/thread.h
@@ -45,6 +45,7 @@ class Thread {
   std::condition_variable cv;
   size_t idx;
   bool exit = false, searching = true; // Set before starting std::thread
+  std::atomic<bool> terminating = false;
   NativeThread stdThread;
 
 public:
