4cd1f5e9996f75c730d746b8c7e2e03abf7e2d5e
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-24 00:05:21 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -969,7 +969,6 @@ moves_loop:  // When in check, search starts here
                
                 if (!givesCheck
                     &&  to_sq(move) != prevSq
-                    &&  type_of(move) != PROMOTION
                     &&  moveCount >= 4 + 2 * depth * depth)
                    continue;
               
