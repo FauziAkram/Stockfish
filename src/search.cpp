6ac18719d7805a7b493f60398c1c579250a012bf
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-23 23:14:26 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1099,6 +1099,9 @@ moves_loop:  // When in check, search starts here
         if ((ss - 1)->moveCount > 7)
             r--;
 
+        if (moveCountPruning && !improving)
+            r++;
+
         // Increase reduction for cut nodes (~3 Elo)
         if (cutNode)
             r += 2;
