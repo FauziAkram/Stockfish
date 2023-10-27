a1b3be778669d7180a9cf95d24c7b1de74030d2c
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-27 20:29:08 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1119,7 +1119,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes (~3 Elo)
         if (cutNode)
-            r += 2;
+            r += 2 - (depth < 8 && ttMove);
 
         // Increase reduction if ttMove is a capture (~3 Elo)
         if (ttCapture)
