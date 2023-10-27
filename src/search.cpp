eabbe99acbcaccf8a12325ce07813999e404a7c7
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-27 21:16:09 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1145,6 +1145,9 @@ moves_loop:  // When in check, search starts here
         else if (move == ttMove)
             r--;
 
+        else
+            r++;
+
         ss->statScore = 2 * thisThread->mainHistory[us][from_to(move)]
                       + (*contHist[0])[movedPiece][to_sq(move)]
                       + (*contHist[1])[movedPiece][to_sq(move)]
