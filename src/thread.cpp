9e1b3c982271fb9a7ad4e3870779a46359ffd458
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-30 18:15:01 +0300

    Update thread.cpp

--- src/thread.cpp
+++ src/thread.cpp
@@ -67,7 +67,7 @@ void Thread::clear() {
 
     counterMoves.fill(MOVE_NONE);
     mainHistory.fill(0);
-    captureHistory.fill(56);
+    captureHistory.fill(84);
     pawnHistory.fill(0);
 
     for (bool inCheck : {false, true})
