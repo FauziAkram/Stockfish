4f2eccab47793051f33867a3a3d6f846ebf48d01
FauziAkram <fauzi.dabat@hotmail.com>
2023-10-31 17:42:03 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -968,7 +968,7 @@ moves_loop:  // When in check, search starts here
 
         Value delta = beta - alpha;
 
-        Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
+        Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta, capture);
 
         // Step 14. Pruning at shallow depth (~120 Elo).
         // Depth conditions are important for mate finding.
