2f1376a5f9785a84e9f9164064ac756ac9749b49
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-06 15:56:17 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1163,8 +1163,8 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
         r -= ss->statScore / (10216 + 3855 * (depth > 5 && depth < 23));
 
-        if (extension < -2)
-            r += 2;
+        if (extension < -1)
+            r -= extension + 1;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         // We use various heuristics for the sons of a node after the first son has
