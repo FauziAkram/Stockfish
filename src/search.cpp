3b59e8438fd3022c473f6f58581c100ac1479407
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-07 19:37:09 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1093,7 +1093,7 @@ moves_loop:  // When in check, search starts here
             }
 
             // Check extensions (~1 Elo)
-            else if (givesCheck && depth > 9)
+            else if (givesCheck && depth > 9 && move == ss->killers[0])
                 extension = 1;
 
             // Quiet ttMove extensions (~1 Elo)
