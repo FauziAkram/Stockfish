0d34ccfb17ac0edb692892461abc4169abd77c15
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-10 23:17:34 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -821,7 +821,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
             if (v >= beta)
                 return nullValue;
         }
-        else if (depth > 1 && depth < 14)
+        else if (depth > 1 && depth < 15)
             depth--;
     }
 
