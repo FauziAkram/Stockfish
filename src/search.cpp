f8ed4cf92250350883342771556e32919c7ce62e
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-11 15:04:35 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1130,7 +1130,7 @@ moves_loop:  // When in check, search starts here
             r += 2;
 
         // Increase reduction if ttMove is a capture (~3 Elo)
-        if (ttCapture && priorCapture)
+        if (ttCapture && !capture)
             r++;
 
         // Decrease reduction for PvNodes (~2 Elo)
