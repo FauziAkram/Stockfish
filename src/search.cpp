032347c9b3940ed9f08556cc4ffdd9b7caecdada
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-11 18:47:36 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -372,8 +372,8 @@ void Thread::search() {
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
             // Adjust optimism based on root move's averageScore (~4 Elo)
-            optimism[us]  = 103 * avg / (std::abs(avg) + 119);
-            optimism[~us] = -116 * avg / (std::abs(avg) + 123);
+            optimism[us]  = 106 * (avg + 38) / (std::abs(avg + 28) + 121);
+            optimism[~us] = -107 * (avg + 38) / (std::abs(avg + 14) + 124);
 
             // Start with a small aspiration window and, in the case of a fail
             // high/low, re-search with a bigger window until we don't fail
