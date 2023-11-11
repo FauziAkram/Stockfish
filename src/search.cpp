1e307b9de5d104c8e5a7e098643ad25dd469329f
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-11 13:40:14 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1002,12 +1002,12 @@ moves_loop:  // When in check, search starts here
                             + thisThread->pawnHistory[pawn_structure(pos)][movedPiece][to_sq(move)];
 
                 // Continuation history based pruning (~2 Elo)
-                if (lmrDepth < 6 && history < -3645 * depth)
+                if (lmrDepth < 6 && history < -886 * depth * depth)
                     continue;
 
                 history += 2 * thisThread->mainHistory[us][from_to(move)];
 
-                lmrDepth += history / 7836;
+                lmrDepth += history / 7800;
                 lmrDepth = std::max(lmrDepth, -1);
 
                 // Futility pruning: parent node (~13 Elo)
