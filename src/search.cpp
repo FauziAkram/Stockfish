ebcb431da5a2efe22d2facbbd43b09379d3b1fa5
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-12 14:56:49 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1341,7 +1341,7 @@ moves_loop:  // When in check, search starts here
         int bonus = (depth > 6) + (PvNode || cutNode) + (bestValue < alpha - 689)
                   + ((ss - 1)->moveCount > 10);
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
-        std::max(depth > 2 ? (depth - 2) * 232 : 0, stat_bonus(depth) * bonus));
+        std::max(depth > 2 ? (depth - 2) * 232 : 232, stat_bonus(depth) * bonus));
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)]
           << stat_bonus(depth) * bonus / 2;
     }
