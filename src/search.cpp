578ae199b40de78b11ba316faf93e1b94fcfd90d
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-13 14:47:10 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -81,10 +81,10 @@ Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
 }
 
 // Reductions lookup table initialized at startup
-int Reductions[MAX_MOVES];  // [depth or moveNumber]
+int Reductions[MAX_DEPTH][MAX_MOVES];  // [depth or moveNumber]
 
 Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
-    int reductionScale = Reductions[d] * Reductions[mn];
+    int reductionScale = Reductions[d][mn];
     return (reductionScale + 1487 - int(delta) * 976 / int(rootDelta)) / 1024
          + (!i && reductionScale > 808);
 }
@@ -185,8 +185,13 @@ uint64_t perft(Position& pos, Depth depth) {
 // Called at startup to initialize various lookup tables
 void Search::init() {
 
-    for (int i = 1; i < MAX_MOVES; ++i)
-        Reductions[i] = int((20.37 + std::log(Threads.size()) / 2) * std::log(i));
+    for (int d = 1; d < MAX_DEPTH; ++d)
+    {
+        for (int i = 1; i < MAX_MOVES; ++i)
+        {
+            Reductions[d][i] = int((20.37 + std::log(Threads.size()) / 2) * std::log(i));
+        }
+    }
 }
 
 
