431c6db3b3851675db819c021fdad27afd30daa5
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-16 02:23:06 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1107,8 +1107,8 @@ moves_loop:  // When in check, search starts here
                           > 4000)
                 extension = 1;
 
-            if (ss->staticEval > (ss-2)->staticEval && (ss-2)->staticEval > (ss-4)->staticEval)
-                extension++; 
+            if (ss->staticEval < (ss-2)->staticEval && (ss-2)->staticEval < (ss-4)->staticEval)
+                extension--;
         }
 
         // Add extension to new depth
