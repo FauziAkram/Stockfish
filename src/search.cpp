259e4836aa9df3a20d0fecda6f9ff13dee35598f
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-19 17:42:29 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -46,6 +46,8 @@
 #include "uci.h"
 
 namespace Stockfish {
+int xx1=51, xx2=100, xx3=100, xx4=100;
+TUNE(xx1,xx2,xx3,xx4);
 
 namespace Search {
 
@@ -1189,13 +1191,10 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result
                 // was good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch     = value > (bestValue + 51 + 10 * (newDepth - d));
-                const bool doEvenDeeperSearch = value > alpha + 700 && ss->doubleExtensions <= 6;
-                const bool doShallowerSearch  = value < bestValue + newDepth;
+                const bool doDeeperSearch     = value > (bestValue + xx1 + (xx2 * r / 10));
+                const bool doShallowerSearch  = value < (bestValue + (xx3 * d / 100) + (xx4 * r / 100));
 
-                ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
-
-                newDepth += doDeeperSearch - doShallowerSearch + doEvenDeeperSearch;
+                newDepth += doDeeperSearch - doShallowerSearch;
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
