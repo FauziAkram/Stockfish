6b3b14dc4c4331e6952b3f77a2cb6aed42081672
FauziAkram <fauzi.dabat@hotmail.com>
2023-11-22 15:29:39 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -898,9 +898,9 @@ moves_loop:  // When in check, search starts here
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
     probCutBeta = beta + 416;
     if (ss->inCheck && !PvNode && ttCapture && (tte->bound() & BOUND_LOWER)
-        && tte->depth() >= depth - 4 && ttValue >= probCutBeta
+        && tte->depth() >= depth - 4
         && abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
-        return probCutBeta;
+        return std::min(ttValue, probCutBeta);
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
                                         (ss - 2)->continuationHistory,
