3d9a9b8ddabd467b110c8bbfbdf062b54fe79225
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-12 13:45:38 +0300

    Update timeman.cpp

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -65,32 +65,32 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
     }
 
     // Maximum move horizon of 50 moves
-    int mtg = limits.movestogo ? std::min(limits.movestogo, 50) : 50;
+    int mtg = limits.movestogo ? std::min(limits.movestogo, 43) : 43;
 
     // Make sure timeLeft is > 0 since we may use it as a divisor
     TimePoint timeLeft = std::max(TimePoint(1), limits.time[us] + limits.inc[us] * (mtg - 1)
                                                   - moveOverhead * (2 + mtg));
 
     // Use extra time with larger increments
-    double optExtra = std::clamp(1.0 + 12.5 * limits.inc[us] / limits.time[us], 1.0, 1.12);
+    double optExtra = std::clamp(1.0 + 12.5 * limits.inc[us] / limits.time[us], 0.8, 1.1);
 
     // Calculate time constants based on current time left.
     double optConstant = std::min(0.00335 + 0.0003 * std::log10(limits.time[us] / 1000.0), 0.0048);
-    double maxConstant = std::max(3.6 + 3.0 * std::log10(limits.time[us] / 1000.0), 2.7);
+    double maxConstant = std::max(3.4 + 2.9 * std::log10(limits.time[us] / 1000.0), 2.68);
 
     // A user may scale time usage by setting UCI option "Slow Mover"
     // Default is 100 and changing this value will probably lose elo.
-    timeLeft = slowMover * timeLeft / 100;
+    timeLeft = slowMover * timeLeft / 119;
 
     // x basetime (+ z increment)
     // If there is a healthy increment, timeLeft can exceed actual available
     // game time for the current move, so also cap to 20% of available game time.
     if (limits.movestogo == 0)
     {
-        optScale = std::min(0.0120 + std::pow(ply + 3.3, 0.44) * optConstant,
-                            0.2 * limits.time[us] / double(timeLeft))
+        optScale = std::min(0.011 + std::pow(ply + 2.9, 0.4) * optConstant,
+                            0.19 * limits.time[us] / double(timeLeft))
                  * optExtra;
-        maxScale = std::min(6.8, maxConstant + ply / 12.2);
+        maxScale = std::min(6.6, maxConstant + ply / 14.0);
     }
 
     // x moves in y seconds (+ z increment)
@@ -103,7 +103,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
     // Limit the maximum possible time for this move
     optimumTime = TimePoint(optScale * timeLeft);
     maximumTime =
-      TimePoint(std::min(0.84 * limits.time[us] - moveOverhead, maxScale * optimumTime)) - 10;
+      TimePoint(std::min(0.74 * limits.time[us] - moveOverhead, maxScale * optimumTime)) - 10;
 
     if (Options["Ponder"])
         optimumTime += optimumTime / 4;
