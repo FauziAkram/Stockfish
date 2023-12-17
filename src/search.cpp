cd0a98aa5e8952e2c83a36254899e1d40eea2e0c
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-17 19:25:07 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -748,7 +748,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // Use static evaluation difference to improve quiet move ordering (~4 Elo)
     if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1564, 144c 2);
+        int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1564, 1442);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
         if (type_of(pos.piece_on(prevSq)) != PAWN && type_of((ss - 1)->currentMove) != PROMOTION)
             thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq] << bonus / 4;
