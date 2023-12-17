7ddf20ffe2c92b85a9514ccf7bf5784314a677fc
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-17 20:00:43 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1103,9 +1103,9 @@ moves_loop:  // When in check, search starts here
                 extension = 1;
 
             // Recapture extensions (~1 Elo)
-            else if (PvNode && move == ttMove && to_sq(move) == prevSq
+            else if (PvNode && !improving && move == ttMove && to_sq(move) == prevSq
                      && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))]
-                          > 4146)
+                          > 4000)
                 extension = 1;
         }
 
