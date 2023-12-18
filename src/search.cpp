8254e5a4e7b4e31522a39134818003669c37c7f5
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-18 17:38:59 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -823,7 +823,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
             if (v >= beta)
                 return nullValue;
         }
-      else if (depth > 1 && thisThread->nodes < 300000)
+      else if (depth > 1 && thisThread->nodes < 400000)
             depth--;
     }
 
