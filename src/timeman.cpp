f8793cce39ed8718be6ed75aee7060a90d4b0d51
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-19 20:46:46 +0300

    Update timeman.cpp

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -63,19 +63,19 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
         limits.npmsec = npmsec;
     }
 
-    // Maximum move horizon of 50 moves
-    int mtg = limits.movestogo ? std::min(limits.movestogo, 50) : 50;
+    // Maximum move horizon
+    int mtg = limits.movestogo ? std::min(limits.movestogo, 48) : 48;
 
     // Make sure timeLeft is > 0 since we may use it as a divisor
     TimePoint timeLeft = std::max(TimePoint(1), limits.time[us] + limits.inc[us] * (mtg - 1)
                                                   - moveOverhead * (2 + mtg));
 
     // Use extra time with larger increments
-    double optExtra = std::clamp(1.0 + 12.5 * limits.inc[us] / limits.time[us], 1.0, 1.11);
+    double optExtra = std::clamp(1.0 + 12.4 * limits.inc[us] / limits.time[us], 1.0, 1.11);
 
     // Calculate time constants based on current time left.
     double optConstant = std::min(0.00334 + 0.0003 * std::log10(limits.time[us] / 1000.0), 0.0049);
-    double maxConstant = std::max(3.4 + 3.0 * std::log10(limits.time[us] / 1000.0), 2.76);
+    double maxConstant = std::max(3.3 + 3.0 * std::log10(limits.time[us] / 1000.0), 2.76);
 
     // x basetime (+ z increment)
     // If there is a healthy increment, timeLeft can exceed actual available
@@ -83,9 +83,9 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
     if (limits.movestogo == 0)
     {
         optScale = std::min(0.0120 + std::pow(ply + 3.1, 0.44) * optConstant,
-                            0.21 * limits.time[us] / double(timeLeft))
+                            0.2 * limits.time[us] / double(timeLeft))
                  * optExtra;
-        maxScale = std::min(6.9, maxConstant + ply / 12.2);
+        maxScale = std::min(6.92, maxConstant + ply / 12.2);
     }
 
     // x moves in y seconds (+ z increment)
