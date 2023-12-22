6e4158a4d1a926b78b689238fc5d2378d6ca3cd1
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-22 17:02:16 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -166,8 +166,8 @@ Value Eval::evaluate(const Position& pos) {
     int   simpleEval = simple_eval(pos, stm) + (int(pos.key() & 7) - 3);
 
     bool lazy = std::abs(simpleEval) >= RookValue + KnightValue + 16 * shuffling * shuffling
-                                          + std::abs(pos.this_thread()->bestValue)
-                                          + std::abs(pos.this_thread()->rootSimpleEval);
+                                          + std::max(std::abs(pos.this_thread()->bestValue),
+                                            std::abs(pos.this_thread()->rootSimpleEval));
 
     if (lazy)
         v = Value(simpleEval);
@@ -183,11 +183,11 @@ Value Eval::evaluate(const Position& pos) {
         nnue -= nnue * (nnueComplexity + std::abs(simpleEval - nnue)) / 32768;
 
         int npm = pos.non_pawn_material() / 64;
-        v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
+        v       = (nnue * (918 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
     }
 
     // Damp down the evaluation linearly when shuffling
-    v = v * (200 - shuffling) / 214;
+    v = v * (200 - shuffling) / 216;
 
     // Guarantee evaluation does not hit the tablebase range
     v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
