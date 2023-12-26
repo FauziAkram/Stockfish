8530bb1f39f1475995a1ccc04f16072e265fee84
FauziAkram <fauzi.dabat@hotmail.com>
2023-12-26 17:55:53 +0300

    Update timeman.cpp

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -74,7 +74,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
     double optExtra = std::clamp(1.0 + 12.5 * limits.inc[us] / limits.time[us], 1.0, 1.11);
 
     // Calculate time constants based on current time left.
-    double optConstant = std::min(0.00334 + 0.0003 * std::log10(limits.time[us] / 1000.0), 0.0049);
+    double optConstant = 0.00334 + 0.0003 * std::log10(limits.time[us] / 1000.0);
     double maxConstant = std::max(3.4 + 3.0 * std::log10(limits.time[us] / 1000.0), 2.76);
 
     // x basetime (+ z increment)
