05eb5b01978da9cd2a5d1c12a802567aaa837186
FauziAkram <fauzi.dabat@hotmail.com>
2024-01-02 17:35:32 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop:  // When in check, search starts here
             r--;
 
         // Decrease reduction if a quiet ttMove has been singularly extended (~1 Elo)
-        if (singularQuietLMR)
+        if (singularQuietLMR && pos.rule50_count() < 10)
             r--;
 
         // Increase reduction on repetition (~1 Elo)
