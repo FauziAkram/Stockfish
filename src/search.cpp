7c12fb7398b551b0a93f622fb2fe6ed22aa69ce6
FauziAkram <fauzi.dabat@hotmail.com>
2024-01-04 12:59:53 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1039,7 +1039,7 @@ moves_loop:  // When in check, search starts here
 
                 // Futility pruning: parent node (~13 Elo)
                 if (!ss->inCheck && lmrDepth < 14
-                    && ss->staticEval + (bestValue < ss->staticEval - 57 ? 124 : 71)
+                    && ss->staticEval + (bestValue < ss->staticEval - 57 ? 136 + 60 * improving : 69)
                            + 118 * lmrDepth
                          <= alpha)
                     continue;
