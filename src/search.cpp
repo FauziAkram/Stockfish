97e93cc157f2e44215491f410f5062863bb01e14
FauziAkram <fauzi.dabat@hotmail.com>
2024-01-11 18:44:47 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -46,6 +46,8 @@
 #include "uci.h"
 
 namespace Stockfish {
+int xx1= 14767, xx2= -14767, zz1=337, zz2=17496, zz3=18782;
+TUNE(xx1,xx2,zz1,zz2,zz3);
 
 namespace Search {
 
@@ -807,14 +809,14 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // The depth condition is important for mate finding.
     if (!ss->ttPv && depth < 9
         && eval - futility_margin(depth, cutNode && !ss->ttHit, improving)
-               - (ss - 1)->statScore / 337
+               - (ss - 1)->statScore / zz1
              >= beta
         && eval >= beta && eval < 29008  // smaller than TB wins
         && (!ttMove || ttCapture))
         return beta > VALUE_TB_LOSS_IN_MAX_PLY ? (eval + beta) / 2 : eval;
 
     // Step 9. Null move search with verification search (~35 Elo)
-    if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 17496
+    if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < zz2
         && eval >= beta && eval >= ss->staticEval && ss->staticEval >= beta - 23 * depth + 304
         && !excludedMove && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
@@ -1199,7 +1201,11 @@ moves_loop:  // When in check, search starts here
                       + (*contHist[3])[movedPiece][move.to_sq()] - 3817;
 
         // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
-        r -= ss->statScore / 14767;
+        if ss->statScore >= xx1
+        r -= ss->statScore / xx1;
+
+        if ss->statScore <= xx2
+        r += ss->statScore / xx2;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         // We use various heuristics for the sons of a node after the first son has
@@ -1377,7 +1383,7 @@ moves_loop:  // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonus = (depth > 6) + (PvNode || cutNode) + ((ss - 1)->statScore < -18782)
+        int bonus = (depth > 6) + (PvNode || cutNode) + ((ss - 1)->statScore < -zz3)
                   + ((ss - 1)->moveCount > 10);
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
                                       stat_bonus(depth) * bonus);
