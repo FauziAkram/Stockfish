a41510dbf9c75483a85b9b0cdb085bceac887f00
FauziAkram <fauzi.dabat@hotmail.com>
2024-01-11 18:48:19 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1201,10 +1201,10 @@ moves_loop:  // When in check, search starts here
                       + (*contHist[3])[movedPiece][move.to_sq()] - 3817;
 
         // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
-        if ss->statScore >= xx1
+        if (ss->statScore >= xx1)
         r -= ss->statScore / xx1;
 
-        if ss->statScore <= xx2
+        if (ss->statScore <= xx2)
         r += ss->statScore / xx2;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
