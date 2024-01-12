8696bec74483855fce7c75aa38ffccdf0b8a3292
FauziAkram <fauzi.dabat@hotmail.com>
2024-01-12 15:42:25 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1592,10 +1592,6 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
 
                 Piece pieceToBeCaptured = pos.piece_on(move.to_sq());
                 futilityValue = futilityBase + PieceValue[pieceToBeCaptured]
-                             + 13 * (type_of(pieceToBeCaptured) == PAWN)
-                             - 13 * (type_of(pieceToBeCaptured) == KNIGHT)
-                             + 12 * (type_of(pieceToBeCaptured) == BISHOP)
-                             + 4 * (type_of(pieceToBeCaptured) == ROOK)
 
                 // If static eval + value of piece we are going to capture is much lower
                 // than alpha we can prune this move.
