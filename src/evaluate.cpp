235b1734e3c5c5eb656cc568d3d4a66e79460431
FauziAkram <fauzi.dabat@hotmail.com>
2024-01-28 01:05:18 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -221,7 +221,7 @@ Value Eval::evaluate(const Position& pos, int optimism) {
         nnue -= nnue * (nnueComplexity + std::abs(simpleEval - nnue)) / 32768;
 
         int npm = pos.non_pawn_material() / 64;
-        v       = (nnue * (914 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
+        v       = (nnue * (928 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
     }
 
     // Damp down the evaluation linearly when shuffling
