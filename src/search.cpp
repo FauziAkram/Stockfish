1923a800627846b25d472e97b82edb32ce4161d7
FauziAkram <fauzi.dabat@hotmail.com>
2024-02-05 17:48:12 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -60,7 +60,7 @@ Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
 }
 
 constexpr int futility_move_count(bool improving, bool worsening, Depth depth) {
-    return improving ? (3 + depth * depth) : worsening ? (3 + depth * depth) / 4 : (3 + depth * depth) / 2;
+    return improving ? (3 + depth * depth) : worsening ? (3 + depth * depth) / 3 : (3 + depth * depth) / 2;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation does not hit the tablebase range
