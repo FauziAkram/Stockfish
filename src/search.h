bb9103045f41f5b7e7e9f5c1cf10b0ac6f54e7fa
FauziAkram <fauzi.dabat@hotmail.com>
2024-02-06 14:13:45 +0300

    Update search.h

--- src/search.h
+++ src/search.h
@@ -212,8 +212,8 @@ class Worker {
 
     Depth reduction(bool i, Depth d, int mn, int delta) {
         int reductionScale = reductions[d] * reductions[mn];
-        return (reductionScale + 1177 - int(delta) * 776 / int(rootDelta)) / 1024
-             + (!i && reductionScale > 842);
+        return (reductionScale + 1188 - int(delta) * 792 / int(rootDelta)) / 1024
+             + (!i && reductionScale > 863);
     }
 
     // Get a pointer to the search manager, only allowed to be called by the
