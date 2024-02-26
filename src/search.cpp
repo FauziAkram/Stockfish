54289b01137ecd1c3b857d6faf5fe538eb05782c
FauziAkram <fauzi.dabat@hotmail.com>
2024-02-26 22:34:55 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -800,7 +800,7 @@ Value Search::Worker::search(
     // Step 8. Futility pruning: child node (~40 Elo)
     // The depth condition is important for mate finding.
     if (!ss->ttPv && depth < 11
-        && eval - futility_margin(depth, cutNode && !ss->ttHit, improving)
+        && eval - futility_margin(depth, cutNode && !ss->ttHit, ss)
                - (ss - 1)->statScore / 314
              >= beta
         && eval >= beta && eval < 30016  // smaller than TB wins
