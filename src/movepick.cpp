f8bee8bbd731d253e729bed7d76a6f64eec5ff91
FauziAkram <fauzi.dabat@hotmail.com>
2024-02-29 01:32:35 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -66,11 +66,12 @@ void partial_insertion_sort(ExtMove* begin, ExtMove* end, int limit) {
     for (ExtMove *sortedEnd = begin, *p = begin + 1; p < end; ++p)
         if (p->value >= limit)
         {
-            ExtMove tmp = *p, *q;
-            *p          = *++sortedEnd;
-            for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
-                *q = *(q - 1);
+            ExtMove tmp = *p;
+            ExtMove* q = sortedEnd;
+            while (q != begin && *(q - 1) < tmp)
+                *q-- = *(q - 1);
             *q = tmp;
+            ++sortedEnd;
         }
 }
 
