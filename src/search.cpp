3b703482571ccbdd67c50c692a91a47281b6db9c
FauziAkram <fauzi.dabat@hotmail.com>
2024-02-29 14:44:09 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -57,7 +57,7 @@ namespace {
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
     Value futilityMult = 117 - 44 * noTtCutNode;
-    return (futilityMult * d - 3 * futilityMult / 2 * improving);
+    return (futilityMult * d - 3 * futilityMult / 2 * (improving || (ss - 2)->staticEval == VALUE_NONE));
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
