e6d31827f21426f3bee81324ceee74712baffeb0
FauziAkram <fauzi.dabat@hotmail.com>
2024-03-12 19:33:48 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -755,7 +755,8 @@ Value Search::Worker::search(
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
     // Adjust razor margin according to cutoffCnt. (~1 Elo)
-    if (eval < alpha - 462 - (296 - 145 * ((ss + 1)->cutoffCnt > 3)) * depth * depth)
+    if (   !PvNode
+       && eval < alpha - 462 - (296 - 145 * ((ss + 1)->cutoffCnt > 3)) * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
