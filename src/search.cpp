6785bcda68fceca3da39583a0cd3f72ca035b17e
FauziAkram <fauzi.dabat@hotmail.com>
2024-03-20 18:43:38 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -74,10 +74,10 @@ Value to_corrected_static_eval(Value v, const Worker& w, const Position& pos) {
 }
 
 // History and stats update bonus, based on depth
-int stat_bonus(Depth d) { return (d == 1 ? 0 : d < 7 ? 245 * d - 320 : 1296); }
+int stat_bonus(Depth d) { return (d == 1 ? 12 : d < 7 ? 258 * d - 320 : 1286); }
 
 // History and stats update malus, based on depth
-int stat_malus(Depth d) { return (d < 4 ? 554 * d - 303 : 1203); }
+int stat_malus(Depth d) { return (d < 4 ? 569 * d - 305 : 1400); }
 
 // Add a small random component to draw evaluations to avoid 3-fold blindness
 Value value_draw(size_t nodes) { return VALUE_DRAW - 1 + Value(nodes & 0x2); }
@@ -1709,7 +1709,7 @@ void update_all_stats(const Position& pos,
 
     if (!pos.capture_stage(bestMove))
     {
-        int bestMoveBonus = bestValue > beta + 168 ? quietMoveBonus      // larger bonus
+        int bestMoveBonus = bestValue > beta + 177 ? quietMoveBonus      // larger bonus
                                                    : stat_bonus(depth);  // smaller bonus
 
         // Increase stats for the best move in case it was a quiet move
