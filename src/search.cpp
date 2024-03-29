f03b348af7165d984fad7d1df08826bf4edfc8ab
FauziAkram <fauzi.dabat@hotmail.com>
2024-03-29 15:04:27 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -999,7 +999,7 @@ moves_loop:  // When in check, search starts here
                   ss->staticEval + (bestValue < ss->staticEval - 59 ? 141 : 58) + 125 * lmrDepth;
 
                 // Futility pruning: parent node (~13 Elo)
-                if (!ss->inCheck && lmrDepth < 15 && futilityValue <= alpha)
+                if (!ss->inCheck && lmrDepth < 14 && futilityValue <= alpha)
                 {
                     if (bestValue <= futilityValue && abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY
                         && futilityValue < VALUE_TB_WIN_IN_MAX_PLY)
