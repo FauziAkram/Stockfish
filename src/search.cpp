94ded777f01e7a5dd047e05c67e4de5c26a0a1db
FauziAkram <fauzi.dabat@hotmail.com>
2024-03-31 14:09:01 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1285,6 +1285,8 @@ moves_loop:  // When in check, search starts here
                     alpha = value;  // Update alpha! Always alpha < beta
                 }
             }
+            else if (   rootNode && !capture && move != ss->killers[0] && move != ss->killers[1])
+              break;
         }
 
         // If the move is worse than some previously searched move,
