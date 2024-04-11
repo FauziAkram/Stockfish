2a33254f10610f2e196e992a89c89b443e90a3a4
FauziAkram <fauzi.dabat@hotmail.com>
2024-04-11 14:12:46 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -45,14 +45,7 @@
 #include "ucioption.h"
 
 namespace Stockfish {
-int xx1=14, xx2=1644, xx3=1384, xx4=0, xx5=200, xx6=50, xx7=50;
-int yy1=14, yy2=1644, yy3=1384, yy4=0, yy5=200, yy6=50, yy7=50;
-TUNE(xx1,xx2,xx3);
-TUNE(SetRange(-4, 6), xx4);
-TUNE(SetRange(-100, 500), xx5,xx6,xx7);
-TUNE(yy1,yy2,yy3);
-TUNE(SetRange(-4, 6), yy4);
-TUNE(SetRange(-100, 400), yy5,yy6,yy7);
+
 namespace TB = Tablebases;
 
 using Eval::evaluate;
@@ -740,21 +733,12 @@ Value Search::Worker::search(
     // Use static evaluation difference to improve quiet move ordering (~9 Elo)
     if (((ss - 1)->currentMove).is_ok() && !(ss - 1)->inCheck && !priorCapture)
     {
-      if (PvNode) {
-        int bonus = std::clamp(-xx1 * int((ss - 1)->staticEval + ss->staticEval), -xx2, xx3);
-        bonus     = bonus > xx4 ? bonus * xx5/100 : bonus * xx6/100;
+      int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1652, 1386);
+        bonus     = bonus > 0 ? 2 * bonus : PvNode? 3 * bonus / 5: 2 * bonus / 5;
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus;
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
-              << bonus * xx7/100;}
-      else {
-        int bonus = std::clamp(-yy1 * int((ss - 1)->staticEval + ss->staticEval), -yy2, yy3);
-        bonus     = bonus > yy4 ? bonus * yy5/100 : bonus * yy6/100;
-        thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus;
-        if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
-            thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
-              << bonus * yy7/100;}
-          
+              << bonus / 2;
     }
 
     // Set up the improving flag, which is true if current static evaluation is
