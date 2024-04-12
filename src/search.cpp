aa31f20dde7487604bc5ff263b0c3b8e047be392
FauziAkram <fauzi.dabat@hotmail.com>
2024-04-12 17:33:11 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -812,8 +812,9 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < c9
-        && eval >= beta && ss->staticEval >= beta - c10 * depth + c11
-        && !excludedMove && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
+        && eval >= beta && ss->staticEval >= beta - c10 * depth + c11 && !excludedMove
+        && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
+        && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
         assert(eval - beta >= 0);
 
