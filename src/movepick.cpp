42570e3196f0595dd5fac2d03e4546906f22a70b
FauziAkram <fauzi.dabat@hotmail.com>
2024-04-19 01:08:23 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -241,7 +241,7 @@ Move MovePicker::select(Pred filter) {
 // moves left, picking the move with the highest score from a list of generated moves.
 Move MovePicker::next_move(bool skipQuiets) {
 
-    auto quiet_threshold = [](Depth d) { return -3550 * d; };
+    auto quiet_threshold = [](Depth d) { return -3450 * (d + 2); };
 
 top:
     switch (stage)
