32d3f26c9a46830fbc26e07c4bcc5183c4ba6cdb
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-03 23:06:14 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1533,7 +1533,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
             if (!givesCheck && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > ((depth < 2) ? 3 : 2))
+                if (moveCount > ((depth < 3) ? 3 : 2))
                     continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
