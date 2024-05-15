46431328cae75f42d19f19855fe846ba0ceb6457
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-15 03:20:41 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -60,7 +60,7 @@ static constexpr double EvalLevel[10] = {0.981, 0.956, 0.895, 0.949, 0.913,
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorsening) {
     Value futilityMult       = 131 - 48 * noTtCutNode;
-    Value improvingDeduction = 64 * improving * futilityMult / 32;
+    Value improvingDeduction = 2 * improving * futilityMult;
     Value worseningDeduction = 330 * oppWorsening * futilityMult / 1024;
 
     return futilityMult * d - improvingDeduction - worseningDeduction;
