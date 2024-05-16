c4779625a3293d04e65127db4bd3ffc72e53ad72
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-16 04:48:17 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -950,7 +950,7 @@ moves_loop:  // When in check, search starts here
         givesCheck = pos.gives_check(move);
 
         // Calculate new depth for this move
-        newDepth = depth - 1;
+        newDepth = depth - 1 - (depth > 10);
 
         int delta = beta - alpha;
 
