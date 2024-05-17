6322296d9fb1b625e7b92d757f4cea7e2d9c1fff
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-17 13:55:03 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -817,7 +817,7 @@ Value Search::Worker::search(
             if (v >= beta)
                 return nullValue;
             else
-                return v;
+                return (v + beta) / 2;
         }
     }
 
