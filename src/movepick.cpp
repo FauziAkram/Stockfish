319de0c56d8409477570e1c26fed640954a3cd99
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-18 19:13:43 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -188,7 +188,7 @@ void MovePicker::score() {
 
             // bonus for checks
             m.value += bool(pos.check_squares(pt) & to) * 16384
-              * pos.non_pawn_material(pos.side_to_move()));
+              * pos.non_pawn_material(pos.side_to_move());
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
