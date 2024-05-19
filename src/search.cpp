ea6229f715584e6fff5dda7e0faca4f4f775bf09
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-19 15:55:40 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1134,8 +1134,8 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             r--;
       
-        if (ss->statScore < -4000)
-            r++;
+        if (std::abs(ss->statScore) > 8000)
+            r--;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
