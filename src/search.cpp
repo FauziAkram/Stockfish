63907a68d5301ec64f3fa8c7737bc710e954dfbb
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-22 14:25:59 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1340,7 +1340,7 @@ moves_loop:  // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonus = (depth > 4) + (depth > 5) + (PvNode || cutNode) + ((ss - 1)->statScore < -13241)
+        int bonus = (depth > 4) + (depth > 5) + (PvNode || cutNode || ss->ttPv) + ((ss - 1)->statScore < -13241)
                   + ((ss - 1)->moveCount > 10) + (!ss->inCheck && bestValue <= ss->staticEval - 127)
                   + (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 74);
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
