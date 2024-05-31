4e6c1015f634e6db64e9b9e72c52b2677b5267fd
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-31 04:26:20 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1131,8 +1131,8 @@ moves_loop:  // When in check, search starts here
             r -= 1 + (ttValue > alpha) + (tte->depth() >= depth);
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
-        if (PvNode)
-            r--;
+        if (!PvNode)
+            r++;
 
         // These reduction adjustments have no proven non-linear scaling.
 
