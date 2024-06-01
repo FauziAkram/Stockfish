3efa07b35921112e159a847134aeabf401774a6c
FauziAkram <fauzi.dabat@hotmail.com>
2024-06-01 15:47:35 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -881,7 +881,7 @@ Value Search::Worker::search(
 
                 // If the qsearch held, perform the regular search
                 if (value >= probCutBeta)
-                    value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4,
+                    value = -search<NonPV>(pos, ss + 1, -probCutBeta - 1, -probCutBeta, depth - 4,
                                            !cutNode);
 
                 pos.undo_move(move);
