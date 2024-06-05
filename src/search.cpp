b0d93382c56ae3ccfc2635628dd161277a6dd0ef
FauziAkram <fauzi.dabat@hotmail.com>
2024-06-05 03:53:44 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop:  // When in check, search starts here
             r += 2 - (tte->depth() >= depth && ss->ttPv)
                + (!ss->ttPv && move != ttMove && move != ss->killers[0]);
 
-        else if (moveCount > 37)
+        else if (moveCount > 41)
           r--;
 
         // Increase reduction if ttMove is a capture (~3 Elo)
