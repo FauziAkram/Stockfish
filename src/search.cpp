dd585f64dce5ba7e5bd7383642ed94c536daa3e1
FauziAkram <fauzi.dabat@hotmail.com>
2024-06-10 20:35:24 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1126,9 +1126,9 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction if position is or has been on the PV (~7 Elo)
         if (ss->ttPv)
-            r -= 1 + (tte->depth() >= depth);
+            r -= 1 + (ttValue > alpha);
 
-        if (ttValue > alpha)
+        if (tte->depth() >= depth)
             r--;
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
