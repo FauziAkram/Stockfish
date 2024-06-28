efa419268aa4bd2e815aa53fd546a749b3f51923
FauziAkram <fauzi.dabat@hotmail.com>
2024-06-28 18:03:25 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore / 11125;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
-        if (depth >= 2 && moveCount > 1 + rootNode)
+        if (depth >= 2 && !ss->inCheck && moveCount > 1 + rootNode)
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
