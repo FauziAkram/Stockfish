1a513fdad9867a0f2f7e4ad982a5dbcb39566f60
FauziAkram <fauzi.dabat@hotmail.com>
2024-06-30 23:07:22 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -369,7 +369,7 @@ void Search::Worker::iterative_deepening() {
                 else if (bestValue >= beta)
                 {
                     if (bestValue >= beta + delta)
-                        alpha = (alpha + beta) / 2;
+                        alpha = (3 * alpha + beta) / 4;
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
