d87af43163d8a614070e202926b805514348748e
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-06 15:14:29 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
-            r--;
+            r -= 1 + (moveCount > 40 && moveCount < 59) + !capture;
 
         // These reduction adjustments have no proven non-linear scaling.
 
