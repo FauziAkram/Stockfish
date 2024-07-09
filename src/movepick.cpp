9d46a7c37e30a10b55bcc91a3d451a87dcddadab
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-09 13:06:52 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -297,15 +297,16 @@ top:
         [[fallthrough]];
 
     case GOOD_QUIET :
-        if (!skipQuiets && select<Next>([&]() {
-                return *cur != refutations[0] && *cur != refutations[1] && *cur != refutations[2];
-            }))
-        {
-            if ((cur - 1)->value > -7998 || (cur - 1)->value <= quiet_threshold(depth))
+        if (!skipQuiets) {
+            if (select<Next>([&]() {
+                return (*cur != refutations[0] && *cur != refutations[1] && *cur != refutations[2]) && 
+                       ((cur - 1)->value > -7998 || (cur - 1)->value <= quiet_threshold(depth));
+            })) {
                 return *(cur - 1);
+            }
 
             // Remaining quiets are bad
-            beginBadQuiets = cur - 1;
+            beginBadQuiets = cur;
         }
 
         // Prepare the pointers to loop over the bad captures
