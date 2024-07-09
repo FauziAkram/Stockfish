c3f57bd4ed5e8e1c27e5dbf14339c1eec62c5932
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-09 17:23:12 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -381,7 +381,7 @@ void Search::Worker::iterative_deepening() {
                 else
                     break;
 
-                alpha -= delta;
+                alpha -= delta / 3;
                 delta += delta / 3;
 
                 assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
