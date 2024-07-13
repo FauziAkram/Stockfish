daa2e3b9ee16856140891a2490c0767083a67f42
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-13 16:57:08 +0300

    Update timeman.cpp

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -118,7 +118,7 @@ void TimeManagement::init(Search::LimitsType& limits,
                             0.213 * limits.time[us] / timeLeft)
                  * originalTimeAdjust;
 
-        maxScale = std::min(6.11, maxConstant + ply / 12.0);
+        maxScale = std::min(7.17, maxConstant + ply / 12.0);
     }
 
     // x moves in y seconds (+ z increment)
