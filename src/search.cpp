fb419e74b211ce595bdabc73476f4af653ac77d0
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-13 03:13:37 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1364,10 +1364,11 @@ moves_loop:  // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonus = (138 * (depth > 5) + 58 * (PvNode || cutNode) + 160 * ((ss - 1)->moveCount > 8)
+        int bonus = (58 * (PvNode || cutNode) + 160 * ((ss - 1)->moveCount > 8)
                      + 84 * (!ss->inCheck && bestValue <= ss->staticEval - 108)
                      + 153 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 76)
-                     + 32  *  (!(ss - 1)->inCheck && bestValue > -(ss - 1)->staticEval + 76));
+                     + 32  *  (!(ss - 1)->inCheck && bestValue > -(ss - 1)->staticEval + 76)
+                     + 138 * (depth > 5) - 64 * (depth <= 5));
 
         // Proportional to "how much damage we have to undo"
         bonus += std::clamp(-(ss - 1)->statScore / 100, -64, 300);
