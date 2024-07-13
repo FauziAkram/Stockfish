4a582712985dcb05df42cefed1807d31b5714fd6
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-14 01:56:26 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1165,7 +1165,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
-            r += 1 + !(PvNode || cutNode|| (ss + 1)->cutoffCnt > 5);
+            r += 1 + !(PvNode || cutNode|| (ss + 1)->cutoffCnt > 6);
 
         // For first picked move (ttMove) reduce reduction, but never allow
         // reduction to go below 0 (~3 Elo)
