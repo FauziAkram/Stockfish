fc46fe3699be5ce87dd555ed62146a75cc9016a7
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-23 20:56:45 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1162,7 +1162,7 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(1, std::min(newDepth - r + (int(ss->inCheck || ss->staticEval <= beta + 242), newDepth + 1));
+            Depth d = std::max(1, std::min(newDepth - r - (int(!ss->inCheck && ss->staticEval > beta + 242), newDepth + 1));
 
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
 
