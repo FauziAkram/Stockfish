aa2cb130a94590f00acc11b7eb5ff0d6824f2c8b
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-25 17:31:51 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -348,6 +348,7 @@ void Search::Worker::iterative_deepening() {
                 }
                 else if (bestValue >= beta)
                 {
+                    alpha = beta;
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
