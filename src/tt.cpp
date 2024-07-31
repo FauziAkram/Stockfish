97ef9985c6b0c2222b6f34120529f2a030aa79f8
FauziAkram <fauzi.dabat@hotmail.com>
2024-07-31 20:56:49 +0300

    Update tt.cpp

--- src/tt.cpp
+++ src/tt.cpp
@@ -93,10 +93,6 @@ bool TTEntry::is_occupied() const { return bool(depth8); }
 void TTEntry::save(
   Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev, uint8_t generation8) {
 
-    // Preserve the old ttmove if we don't have a new one
-    if (m || uint16_t(k) != key16)
-        move16 = m;
-
     // Overwrite less valuable entries (cheapest checks first)
     if (b == BOUND_EXACT || uint16_t(k) != key16 || d - DEPTH_ENTRY_OFFSET + 2 * pv > depth8 - 4
         || relative_age(generation8))
@@ -110,6 +106,10 @@ void TTEntry::save(
         value16   = int16_t(v);
         eval16    = int16_t(ev);
     }
+
+    // Update move only if necessary
+    if (m || uint16_t(k) != key16)
+        move16 = m;
 }
 
 
