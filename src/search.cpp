ec854714fb620c4b3dd871ac91b91a9668b042c7
FauziAkram <fauzi.dabat@hotmail.com>
2024-08-03 03:27:25 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -877,8 +877,7 @@ Value Search::Worker::search(
                 // Save ProbCut data into transposition table
                 ttWriter.write(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER,
                                depth - 3, move, unadjustedStaticEval, tt.generation());
-                return std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY ? value - (probCutBeta - beta)
-                                                                 : value;
+                return value - (value >= VALUE_TB_WIN_IN_MAX_PLY ? 0 : probCutBeta - beta);
             }
         }
 
