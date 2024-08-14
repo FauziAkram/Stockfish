9475532043df1d48c5b39092c51b0c8516b17263
FauziAkram <fauzi.dabat@hotmail.com>
2024-08-15 02:57:30 +0300

    Update movepick.h

--- src/movepick.h
+++ src/movepick.h
@@ -119,7 +119,7 @@ enum StatsType {
 using ButterflyHistory = Stats<int16_t, 7183, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)>;
 
 // CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-using CapturePieceToHistory = Stats<int16_t, 9837, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using CapturePieceToHistory = Stats<int16_t, 11120, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 // PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
 using PieceToHistory = Stats<int16_t, 29952, PIECE_NB, SQUARE_NB>;
