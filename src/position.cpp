1260374e921ea2eaa825a0226216c798fb5f793e
FauziAkram <fauzi.dabat@hotmail.com>
2024-08-30 03:53:00 +0300

    Update position.cpp

--- src/position.cpp
+++ src/position.cpp
@@ -1122,7 +1122,7 @@ bool Position::see_ge(Move m, int threshold) const {
               // reverse the result.
             return (attackers & ~pieces(stm)) ? res ^ 1 : res;
        if (!res)
-+          swap += 120;
+          swap += 120;
     }
 
     return bool(res);
