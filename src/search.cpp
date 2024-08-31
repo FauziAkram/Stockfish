2b752108012c7288cf9cb2c3394dff69506b5073
FauziAkram <fauzi.dabat@hotmail.com>
2024-08-31 21:01:34 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1550,6 +1550,8 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         // Step 6. Pruning
         if (bestValue > VALUE_TB_LOSS_IN_MAX_PLY && pos.non_pawn_material(us))
         {
+            if (bestMove)
+                    break;
             // Futility pruning and moveCount pruning (~10 Elo)
             if (!givesCheck && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
                 && move.type_of() != PROMOTION)
