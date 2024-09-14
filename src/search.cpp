9593aecf634f1c7db31175d08728cb485e43d58d
FauziAkram <fauzi.dabat@hotmail.com>
2024-09-14 15:12:58 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -822,7 +822,7 @@ Value Search::Worker::search(
         return qsearch<PV>(pos, ss, alpha, beta);
 
     if (!PvNode && ttData.value < alpha && ttData.depth >= depth && ttCapture)
-        depth -= ((ss + 1)->cutoffCnt > 4) + (tte->bound() == BOUND_UPPER);
+        depth -= ((ss + 1)->cutoffCnt > 4) + (ttData.bound == BOUND_UPPER);
 
     if (depth <= 0)
         return qsearch<NonPV>(pos, ss, alpha, beta);
