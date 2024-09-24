f47cc57bdfab684e40f044aada904c294f78e359
FauziAkram <fauzi.dabat@hotmail.com>
2024-09-25 00:04:20 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -275,6 +275,12 @@ void Search::Worker::iterative_deepening() {
 
     rootHistory.fill(0);
 
+    for (Color c : {WHITE, BLACK}) {
+    for (int i = 0; i < SQUARE_NB * SQUARE_NB; ++i) {
+        mainHistory[c][i] = mainHistory[c][i] * 95 / 100;
+    }
+    }
+
     // Iterative deepening loop until requested to stop or the target depth is reached
     while (++rootDepth < MAX_PLY && !threads.stop
            && !(limits.depth && mainThread && rootDepth > limits.depth))
@@ -498,7 +504,7 @@ void Search::Worker::iterative_deepening() {
 
 // Reset histories, usually before a new game
 void Search::Worker::clear() {
-    mainHistory.fill(0);
+    mainHistory.fill(1);
     rootHistory.fill(0);
     captureHistory.fill(-753);
     pawnHistory.fill(-1152);
