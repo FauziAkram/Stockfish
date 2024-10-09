57039b7112ddfdaec6a23e04b1db2551fa99bf04
FauziAkram <fauzi.dabat@hotmail.com>
2024-10-09 14:14:56 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1512,7 +1512,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             // ttValue can be used as a better position evaluation (~13 Elo)
             if (std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY
                 && (ttData.bound & (ttData.value > bestValue ? BOUND_LOWER : BOUND_UPPER)))
-                bestValue = ttData.value;
+                bestValue = ttData.value + 14 - 20 * (ss->ttPv && !PvNode);
         }
         else
         {
