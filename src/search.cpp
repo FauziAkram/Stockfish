a8024f976a8f1bca143e8d04070d3ff792161b11
FauziAkram <fauzi.dabat@hotmail.com>
2024-10-09 14:14:00 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -732,7 +732,7 @@ Value Search::Worker::search(
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (ttData.value != VALUE_NONE
             && (ttData.bound & (ttData.value > eval ? BOUND_LOWER : BOUND_UPPER)))
-            eval = ttData.value;
+            eval = ttData.value - 30 + 10 * (ss->ttPv && !PvNode);
     }
     else
     {
