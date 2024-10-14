ea423c2b6578b666efa8aefa549fb2a7a399c079
FauziAkram <fauzi.dabat@hotmail.com>
2024-10-14 20:28:06 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -147,7 +147,7 @@ void MovePicker::score() {
             m.value =
               7 * int(PieceValue[pos.piece_on(m.to_sq())])
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))]
-              + mainHistory[pos.side_to_move()][m.from_to()] / 8;
+              + int(mainHistory[pos.side_to_move()][m.from_to()]) / 8;
 
         else if constexpr (Type == QUIETS)
         {
