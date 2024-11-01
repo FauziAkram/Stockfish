03b8806efccfc3bc16bdcc3bad2880783fbe05f4
FauziAkram <fauzi.dabat@hotmail.com>
2024-11-01 17:23:35 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1330,10 +1330,9 @@ moves_loop:  // When in check, search starts here
                 else
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
-                    if (depth > 2 && depth < 14 && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
+                    if (depth < 14 && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
                         depth -= 2;
 
-                    assert(depth > 0);
                     alpha = value;  // Update alpha! Always alpha < beta
                 }
             }
