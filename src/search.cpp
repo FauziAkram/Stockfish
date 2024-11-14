4402cc3c70600fc65e47d7498bd2fc2ba5d61bbb
FauziAkram <fauzi.dabat@hotmail.com>
2024-11-14 15:51:34 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1089,8 +1089,8 @@ moves_loop:  // When in check, search starts here
 
                 if (value < singularBeta)
                 {
-                    int doubleMargin = - 43 + 264 * PvNode - 190 * !ttCapture + 11 * cutNode;
-                    int tripleMargin = 112 + 288 * PvNode - 252 * !ttCapture + 97 * ss->ttPv + 18 * cutNode;
+                    int doubleMargin = - 32 + 264 * PvNode - 190 * !ttCapture;
+                    int tripleMargin = 94 + 288 * PvNode - 252 * !ttCapture + 97 * ss->ttPv;
                     int quadMargin   = 454 + 336 * PvNode - 294 * !ttCapture + 234 * ss->ttPv;
 
                     extension = 1 + (value < singularBeta - doubleMargin)
