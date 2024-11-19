4c4431f9e457e8e77262891dae1561985781cfb1
FauziAkram <fauzi.dabat@hotmail.com>
2024-11-19 13:18:09 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -444,9 +444,9 @@ void Search::Worker::iterative_deepening() {
         {
             int nodesEffort = rootMoves[0].effort * 100 / std::max(size_t(1), size_t(nodes));
 
-            double fallingEval = (11 + 2 * (mainThread->bestPreviousAverageScore - bestValue)
+            double fallingEval = (12 + 2 * (mainThread->bestPreviousAverageScore - bestValue)
                                   + (mainThread->iterValue[iterIdx] - bestValue))
-                               / 100.0;
+                               / 98.0;
             fallingEval = std::clamp(fallingEval, 0.580, 1.667);
 
             // If the bestMove is stable over several iterations, reduce time accordingly
