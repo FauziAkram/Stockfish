7a069a86b90bf63bf796ca86a8f60d210e882588
FauziAkram <fauzi.dabat@hotmail.com>
2024-11-24 00:13:20 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1860,7 +1860,7 @@ void update_quiet_histories(
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(), bonus);
 
     int pIndex = pawn_structure_index(pos);
-    workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()] << bonus / 2;
+    workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()] << bonus;
 }
 
 }
