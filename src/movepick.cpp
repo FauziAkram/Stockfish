80a8e968225e8fa5f9d31fe955515da113569fd7
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-01 15:55:12 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -170,15 +170,15 @@ void MovePicker::score() {
             m.value += bool(pos.check_squares(pt) & to) * 16384;
 
             // bonus for escaping from capture
-            m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
-                                                  : pt == ROOK && !(to & threatenedByMinor) ? 25600
-                                                  : !(to & threatenedByPawn)                ? 14450
+            m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51800
+                                                  : pt == ROOK && !(to & threatenedByMinor) ? 26000
+                                                  : !(to & threatenedByPawn)                ? 14500
                                                                                             : 0)
                                                : 0;
 
             // malus for putting piece en prise
             m.value -= (pt == QUEEN ? bool(to & threatenedByRook) * 49000
-                        : pt == ROOK && bool(to & threatenedByMinor) ? 24335
+                        : pt == ROOK && bool(to & threatenedByMinor) ? 24300
                                                                      : 0);
 
             if (ply < LOW_PLY_HISTORY_SIZE)
