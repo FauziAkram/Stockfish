c22d0b0b388214fd53493d17ea8a59b1cec7042c
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-02 17:49:06 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -867,7 +867,7 @@ Value Search::Worker::search(
     {
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
-        MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &thisThread->captureHistory);
+        MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval + 2 * depth, &thisThread->captureHistory);
         Piece      captured;
 
         while ((move = mp.next_move()) != Move::none())
