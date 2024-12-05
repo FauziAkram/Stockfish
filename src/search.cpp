7519569b7153420c8ea0510d63b5ac11e5531122
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-06 02:16:13 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1330,7 +1330,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += !ttData.move + (extension < 2) + (r > 8000);
+                    ss->cutoffCnt += !ttData.move + (extension < 2) - (r > 8000);
                     assert(value >= beta);  // Fail high
                     break;
                 }
