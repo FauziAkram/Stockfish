87cb1586cb17f2e72944a839675746f882d0cd23
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-10 17:37:01 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1337,7 +1337,7 @@ moves_loop:  // When in check, search starts here
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
                     if (depth > 2 && depth < 14 && !is_decisive(value))
-                        depth -= 1 + (depth < 10);
+                        depth -= 1 + (depth < 6);
 
                     assert(depth > 0);
                     alpha = value;  // Update alpha! Always alpha < beta
