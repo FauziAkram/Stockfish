c3b84d41d656785d397e322c1fc6eeaa2c85793a
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-12 14:37:04 +0300

    Update position.h

--- src/position.h
+++ src/position.h
@@ -147,7 +147,7 @@ class Position {
     void undo_null_move();
 
     // Static Exchange Evaluation
-    bool see_ge(Move m, int threshold = 0) const;
+    bool see_ge(Move m, int threshold = 0, bool considerPins = false) const;
 
     // Accessing hash keys
     Key key() const;
