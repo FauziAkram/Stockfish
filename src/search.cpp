65d37680517b7399dd3574de2d6385e3f55da9ed
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-14 23:49:11 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1179,15 +1179,11 @@ moves_loop:  // When in check, search starts here
 - 312 * (thisThread->majorPieceCorrectionHistory[us][major_piece_index(pos)] > 500)
           - 352 * (thisThread->minorPieceCorrectionHistory[us][minor_piece_index(pos)] < -500)
 + 207 * (thisThread->minorPieceCorrectionHistory[us][minor_piece_index(pos)] < 0)
-- 69 * (thisThread->minorPieceCorrectionHistory[us][minor_piece_index(pos)] > 0)
-- 34 * (thisThread->minorPieceCorrectionHistory[us][minor_piece_index(pos)] > 500)
-           - 50 * (thisThread->nonPawnCorrectionHistory[WHITE][us][non_pawn_index<WHITE>(pos)] < -500)
 + 323 * (thisThread->nonPawnCorrectionHistory[WHITE][us][non_pawn_index<WHITE>(pos)] < 0)
 + 252 * (thisThread->nonPawnCorrectionHistory[WHITE][us][non_pawn_index<WHITE>(pos)] > 0)
 - 121 * (thisThread->nonPawnCorrectionHistory[WHITE][us][non_pawn_index<WHITE>(pos)] > 500)
           - 322 * (thisThread->nonPawnCorrectionHistory[BLACK][us][non_pawn_index<BLACK>(pos)] < -500)
 - 328 * (thisThread->nonPawnCorrectionHistory[BLACK][us][non_pawn_index<BLACK>(pos)] < 0)
-+ 78 * (thisThread->nonPawnCorrectionHistory[BLACK][us][non_pawn_index<BLACK>(pos)] > 0)
 - 592 * (thisThread->nonPawnCorrectionHistory[BLACK][us][non_pawn_index<BLACK>(pos)] > 500);
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
