e794897ab6f2f3cf7b1efcc1b3ab26cb58baaec6
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-15 03:41:59 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1166,10 +1166,10 @@ moves_loop:  // When in check, search starts here
             r += 2518 - (ttData.depth >= depth && ss->ttPv) * 991;
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
-        if (ttCapture && !capture)
-            r += 1043 + (depth < 8) * 999;
+        if (ttCapture && !capture) {
+            r += 1043 + (depth < 8) * 999; }
 
-            r += -1024 * (std::abs(thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)]) >= 512);
+            r -= 1024 * (std::abs(thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)]) >= 512);
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
