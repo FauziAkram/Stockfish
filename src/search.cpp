519d425bcf434290ffcce7c872afe93f80d1c119
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-15 03:43:06 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,8 @@ moves_loop:  // When in check, search starts here
         if (ttCapture && !capture)
             r += 1043 + (depth < 8) * 999;
 
-            r -= 1024 * (thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)] <= -512);
+        if (thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)] <= -512)
+            r -= 1024; 
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
