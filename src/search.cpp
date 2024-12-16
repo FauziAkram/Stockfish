61519d87d11bfaa947c102adbbba85c9db7f0bbb
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-16 15:49:36 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -992,7 +992,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-            if ((!excludedMove || depth > 12) && moveCount >= futility_move_count(improving, depth))
+            if ((!excludedMove || depth > 18) && moveCount >= futility_move_count(improving, depth))
                 mp.skip_quiet_moves();
 
             // Reduced depth of the next LMR search
