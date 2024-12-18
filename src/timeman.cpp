cbebf468ccca09c63c527b71a4e018b07247666c
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-18 20:29:50 +0300

    Update timeman.cpp

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -87,8 +87,8 @@ void TimeManagement::init(Search::LimitsType& limits,
     const TimePoint scaledTime  = limits.time[us] / scaleFactor;
     const TimePoint scaledInc   = limits.inc[us] / scaleFactor;
 
-    // Maximum move horizon of 50 moves
-    int mtg = limits.movestogo ? std::min(limits.movestogo, 50) : 50;
+    // Maximum move horizon
+    int mtg = limits.movestogo ? std::min(limits.movestogo, 50 + 10 * (ply < 10)) : 50 + 10 * (ply < 10);
 
     // If less than one second, gradually reduce mtg
     if (scaledTime < 1000 && double(mtg) / scaledInc > 0.05)
