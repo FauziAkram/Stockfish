90b462481d33c4e5ddb07123f84a534a9284386e
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-29 01:28:07 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -931,11 +931,11 @@ moves_loop:  // When in check, search starts here
                                         (ss - 3)->continuationHistory,
                                         (ss - 4)->continuationHistory,
                                         nullptr,
-                                        (ss - 6)->continuationHistory};
-
+                                        (ss - 6)->continuationHistory},
+                                        &thisThread->pawnPromotionHistory[0][0]};
 
     MovePicker mp(pos, ttData.move, depth, &thisThread->mainHistory, &thisThread->lowPlyHistory,
-                  &thisThread->captureHistory, contHist, &thisThread->pawnHistory, ss->ply);
+                  &thisThread->captureHistory, contHist, &thisThread->pawnHistory, &thisThread->pawnPromotionHistory, ss->ply);
 
     value = bestValue;
 
