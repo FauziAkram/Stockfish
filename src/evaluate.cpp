ebdd8e0d24da1bbfad617abfaa5f91944a384c8b
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-29 16:35:07 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -75,7 +75,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     int mobilityBonus = popcount(pawnMobility);
 
     // Combine the modifications into the 'positional' component.
-    positional -= mobilityBonus * 2;
+    positional -= mobilityBonus;
 
     Value nnue = (125 * psqt + 131 * positional) / 128;
 
