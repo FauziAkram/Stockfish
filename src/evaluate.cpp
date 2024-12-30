ab304c3f709bd946d9376db1ad5604909b2422f4
FauziAkram <fauzi.dabat@hotmail.com>
2024-12-30 15:28:16 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -47,7 +47,8 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > 962;
+    int pc = pos.count<ALL_PIECES>();
+    return std::abs(simpleEval) > 1086 + (pc > 22) * 20 + (pc > 18) * 23 + (pc > 9) * 26;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
