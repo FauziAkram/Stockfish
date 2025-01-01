5f296a49d3ba761eb4f1f8d36a55b09aecb847d8
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-01 21:33:26 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -37,7 +37,14 @@
 
 namespace Stockfish {
 int aa1=1000, aa2=1000,aa3=1000, aa4=1000,aa5=1000, aa6=1000,aa7=1000, aa8=1000,aa9=1000, aa10=1000;
-int AA1=1048, AA2=1048,AA3=1048, AA4=1048,AA5=1048, AA6=1048,AA7=1048, AA8=1048,AA9=1048, AA10=1048;
+int bb1=1000, bb2=1000,bb3=1000, bb4=1000,bb5=1000, bb6=1000,bb7=1000, bb8=1000,bb9=1000, bb10=1000;
+int cc1=1000, cc2=1000,cc3=1000, cc4=1000,cc5=1000, cc6=1000,cc7=1000, cc8=1000,cc9=1000, cc10=1000;
+int dd1=1000, dd2=1000,dd3=1000, dd4=1000,dd5=1000, dd6=1000,dd7=1000, dd8=1000,dd9=1000, dd10=1000;
+int ee1=1000, ee2=1000,ee3=1000, ee4=1000,ee5=1000, ee6=1000,ee7=1000, ee8=1000,ee9=1000, ee10=1000;
+int ff1=1000, ff2=1000,ff3=1000, ff4=1000,ff5=1000, ff6=1000,ff7=1000, ff8=1000,ff9=1000, ff10=1000;
+int gg1=1000, gg2=1000,gg3=1000, gg4=1000,gg5=1000, gg6=1000,gg7=1000, gg8=1000,gg9=1000, gg10=1000;
+int hh1=1000, hh2=1000,hh3=1000, hh4=1000,hh5=1000, hh6=1000,hh7=1000, hh8=1000,hh9=1000, hh10=1000;
+int ii1=1000, ii2=1000,ii3=1000, ii4=1000,ii5=1000, ii6=1000,ii7=1000, ii8=1000,ii9=1000, ii10=1000;
 
 // Returns a static, purely materialistic evaluation of the position from
 // the point of view of the given color. It can be divided by PawnValue to get
@@ -64,27 +71,80 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     bool smallNet           = use_smallnet(pos);
     auto [psqt, positional] = smallNet ? networks.small.evaluate(pos, &caches.small)
                                        : networks.big.evaluate(pos, &caches.big);
-if (psqt < -3500){
-  if (positional < -3500)
-    Value nnue = (aa1 * psqt + AA1 * positional) / 1024;
-  else if (positional < -2500)
-    Value nnue = (aa2 * psqt + AA2 * positional) / 1024;
-  else if (positional < -1500)
-    Value nnue = (aa3 * psqt + AA3 * positional) / 1024;
-  else if (positional < -500)
-    Value nnue = (aa4 * psqt + AA4 * positional) / 1024;
-  else if (positional < 500)
-    Value nnue = (aa5 * psqt + AA5 * positional) / 1024;
-  else if (positional < 1500)
-    Value nnue = (aa6 * psqt + AA6 * positional) / 1024;
-  else if (positional < 2500)
-    Value nnue = (aa7 * psqt + AA7 * positional) / 1024;
-  else if (positional < 2500)
-    Value nnue = (aa8 * psqt + AA8 * positional) / 1024;
-  else
-    Value nnue = (aa9 * psqt + AA9 * positional) / 1024;}
-  else
-    Value nnue = (aa10 * psqt + AA10 * positional) / 1024;
+
+    Value nnue = (  (psqt < -3500)?  ((positional < -3500)? aa1:
+                                     (positional < -2500)? aa2:
+                                     (positional < -1500)? aa3:
+                                     (positional < -500)? aa4:
+                                     (positional < 500)? aa5:
+                                     (positional < 1500)? aa6:
+                                     (positional < 2500)? aa7:
+                                     (positional < 3500)? aa8:aa9):
+                     (psqt < -2500)?  ((positional < -3500)? bb1:
+                                     (positional < -2500)? bb2:
+                                     (positional < -1500)? bb3:
+                                     (positional < -500)? bb4:
+                                     (positional < 500)? bb5:
+                                     (positional < 1500)? bb6:
+                                     (positional < 2500)? bb7:
+                                     (positional < 3500)? bb8:bb9):
+                     (psqt < -1500)?  ((positional < -3500)? cc1:
+                                     (positional < -2500)? cc2:
+                                     (positional < -1500)? cc3:
+                                     (positional < -500)? cc4:
+                                     (positional < 500)? cc5:
+                                     (positional < 1500)? cc6:
+                                     (positional < 2500)? cc7:
+                                     (positional < 3500)? cc8:cc9):
+                     (psqt < -500)?  ((positional < -3500)? dd1:
+                                     (positional < -2500)? dd2:
+                                     (positional < -1500)? dd3:
+                                     (positional < -500)? dd4:
+                                     (positional < 500)? dd5:
+                                     (positional < 1500)? dd6:
+                                     (positional < 2500)? dd7:
+                                     (positional < 3500)? dd8:dd9):
+                     (psqt < 500)?  ((positional < -3500)? ee1:
+                                     (positional < -2500)? ee2:
+                                     (positional < -1500)? ee3:
+                                     (positional < -500)? ee4:
+                                     (positional < 500)? ee5:
+                                     (positional < 1500)? ee6:
+                                     (positional < 2500)? ee7:
+                                     (positional < 3500)? ee8:ee9):
+                    (psqt < 1500)?  ((positional < -3500)? ff1:
+                                     (positional < -2500)? ff2:
+                                     (positional < -1500)? ff3:
+                                     (positional < -500)? ff4:
+                                     (positional < 500)? ff5:
+                                     (positional < 1500)? ff6:
+                                     (positional < 2500)? ff7:
+                                     (positional < 3500)? ff8:ff9):
+                    (psqt < 2500)?  ((positional < -3500)? gg1:
+                                     (positional < -2500)? gg2:
+                                     (positional < -1500)? gg3:
+                                     (positional < -500)? gg4:
+                                     (positional < 500)? gg5:
+                                     (positional < 1500)? gg6:
+                                     (positional < 2500)? gg7:
+                                     (positional < 3500)? gg8:gg9):
+                     (psqt < 3500)?  ((positional < -3500)? hh1:
+                                     (positional < -2500)? hh2:
+                                     (positional < -1500)? hh3:
+                                     (positional < -500)? hh4:
+                                     (positional < 500)? hh5:
+                                     (positional < 1500)? hh6:
+                                     (positional < 2500)? hh7:
+                                     (positional < 3500)? hh8:hh9):
+                                    ((positional < -3500)? ii1:
+                                     (positional < -2500)? ii2:
+                                     (positional < -1500)? ii3:
+                                     (positional < -500)? ii4:
+                                     (positional < 500)? ii5:
+                                     (positional < 1500)? ii6:
+                                     (positional < 2500)? ii7:
+                                     (positional < 3500)? ii8:ii9)
+    * psqt + 1048 * positional) / 1024;
 
     // Re-evaluate the position when higher eval accuracy is worth the time spent
     if (smallNet && (std::abs(nnue) < 236))
