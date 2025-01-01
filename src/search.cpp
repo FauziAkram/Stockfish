2bbcd47c979e4a0be7dfaad2561d5f2ae74f1736
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-01 19:50:13 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1207,10 +1207,11 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
+                if (!rootNode) {
                 const bool doDeeperSearch    = value > (bestValue + 42 + 2 * newDepth);  // (~1 Elo)
                 const bool doShallowerSearch = value < bestValue + 10;                   // (~2 Elo)
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                newDepth += doDeeperSearch - doShallowerSearch;}
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
