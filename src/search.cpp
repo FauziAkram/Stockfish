fdf9b7bf534c5f2e7c9fa24525d3dddc6f0be230
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-02 02:42:03 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1708,7 +1708,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
 
 Depth Search::Worker::reduction(bool i, Depth d, int mn) const {
     int reductionScale = reductions[d] * reductions[mn];
-    return reductionScale + !i * reductionScale / 3 + 1204;
+    return reductionScale + !i * reductionScale / 3 + 1264;
 }
 
 // elapsed() returns the time elapsed since the search started. If the
