861ce52839542fab04e08428698f0bbbcf479e62
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-01 22:37:38 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -45,15 +45,7 @@ int ff1=1000, ff2=1000,ff3=1000, ff4=1000,ff5=1000, ff6=1000,ff7=1000, ff8=1000,
 int gg1=1000, gg2=1000,gg3=1000, gg4=1000,gg5=1000, gg6=1000,gg7=1000, gg8=1000,gg9=1000;
 int hh1=1000, hh2=1000,hh3=1000, hh4=1000,hh5=1000, hh6=1000,hh7=1000, hh8=1000,hh9=1000;
 int ii1=1000, ii2=1000,ii3=1000, ii4=1000,ii5=1000, ii6=1000,ii7=1000, ii8=1000,ii9=1000;
-int AA1=1048, AA2=1048,AA3=1048, AA4=1048,AA5=1048, AA6=1048,AA7=1048, AA8=1048,AA9=1048;
-int BB1=1048, BB2=1048,BB3=1048, BB4=1048,BB5=1048, BB6=1048,BB7=1048, BB8=1048,BB9=1048;
-int CC1=1048, CC2=1048,CC3=1048, CC4=1048,CC5=1048, CC6=1048,CC7=1048, CC8=1048,CC9=1048;
-int DD1=1048, DD2=1048,DD3=1048, DD4=1048,DD5=1048, DD6=1048,DD7=1048, DD8=1048,DD9=1048;
-int EE1=1048, EE2=1048,EE3=1048, EE4=1048,EE5=1048, EE6=1048,EE7=1048, EE8=1048,EE9=1048;
-int FF1=1048, FF2=1048,FF3=1048, FF4=1048,FF5=1048, FF6=1048,FF7=1048, FF8=1048,FF9=1048;
-int GG1=1048, GG2=1048,GG3=1048, GG4=1048,GG5=1048, GG6=1048,GG7=1048, GG8=1048,GG9=1048;
-int HH1=1048, HH2=1048,HH3=1048, HH4=1048,HH5=1048, HH6=1048,HH7=1048, HH8=1048,HH9=1048;
-int II1=1048, II2=1048,II3=1048, II4=1048,II5=1048, II6=1048,II7=1048, II8=1048,II9=1048;
+
 TUNE(aa1, aa2,aa3, aa4,aa5, aa6,aa7, aa8,aa9);
 TUNE(bb1, bb2,bb3, bb4,bb5, bb6,bb7, bb8,bb9);
 TUNE(cc1, cc2,cc3, cc4,cc5, cc6,cc7, cc8,cc9);
@@ -63,15 +55,7 @@ TUNE(ff1, ff2,ff3, ff4,ff5, ff6,ff7, ff8,ff9);
 TUNE(gg1, gg2,gg3, gg4,gg5, gg6,gg7, gg8,gg9);
 TUNE(hh1, hh2,hh3, hh4,hh5, hh6,hh7, hh8,hh9);
 TUNE(ii1, ii2,ii3, ii4,ii5, ii6,ii7, ii8,ii9);
-TUNE(AA1, AA2,AA3, AA4,AA5, AA6,AA7, AA8,AA9);
-TUNE(BB1, BB2,BB3, BB4,BB5, BB6,BB7, BB8,BB9);
-TUNE(CC1, CC2,CC3, CC4,CC5, CC6,CC7, CC8,CC9);
-TUNE(DD1, DD2,DD3, DD4,DD5, DD6,DD7, DD8,DD9);
-TUNE(EE1, EE2,EE3, EE4,EE5, EE6,EE7, EE8,EE9);
-TUNE(FF1, FF2,FF3, FF4,FF5, FF6,FF7, FF8,FF9);
-TUNE(GG1, GG2,GG3, GG4,GG5, GG6,GG7, GG8,GG9);
-TUNE(HH1, HH2,HH3, HH4,HH5, HH6,HH7, HH8,HH9);
-TUNE(II1, II2,II3, II4,II5, II6,II7, II8,II9);
+
 
 
 // Returns a static, purely materialistic evaluation of the position from
@@ -173,78 +157,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
                                      (positional < 2500)? ii7:
                                      (positional < 3500)? ii8:ii9)) * psqt;
   
-  Value scaled_positional = ((psqt < -3500)?  ((positional < -3500)? AA1:
-                                     (positional < -2500)? AA2:
-                                     (positional < -1500)? AA3:
-                                     (positional < -500)? AA4:
-                                     (positional < 500)? AA5:
-                                     (positional < 1500)? AA6:
-                                     (positional < 2500)? AA7:
-                                     (positional < 3500)? AA8:AA9):
-                     (psqt < -2500)?  ((positional < -3500)? BB1:
-                                     (positional < -2500)? BB2:
-                                     (positional < -1500)? BB3:
-                                     (positional < -500)? BB4:
-                                     (positional < 500)? BB5:
-                                     (positional < 1500)? BB6:
-                                     (positional < 2500)? BB7:
-                                     (positional < 3500)? BB8:BB9):
-                     (psqt < -1500)?  ((positional < -3500)? CC1:
-                                     (positional < -2500)? CC2:
-                                     (positional < -1500)? CC3:
-                                     (positional < -500)? CC4:
-                                     (positional < 500)? CC5:
-                                     (positional < 1500)? CC6:
-                                     (positional < 2500)? CC7:
-                                     (positional < 3500)? CC8:CC9):
-                     (psqt < -500)?  ((positional < -3500)? DD1:
-                                     (positional < -2500)? DD2:
-                                     (positional < -1500)? DD3:
-                                     (positional < -500)? DD4:
-                                     (positional < 500)? DD5:
-                                     (positional < 1500)? DD6:
-                                     (positional < 2500)? DD7:
-                                     (positional < 3500)? DD8:DD9):
-                     (psqt < 500)?  ((positional < -3500)? EE1:
-                                     (positional < -2500)? EE2:
-                                     (positional < -1500)? EE3:
-                                     (positional < -500)? EE4:
-                                     (positional < 500)? EE5:
-                                     (positional < 1500)? EE6:
-                                     (positional < 2500)? EE7:
-                                     (positional < 3500)? EE8:EE9):
-                    (psqt < 1500)?  ((positional < -3500)? FF1:
-                                     (positional < -2500)? FF2:
-                                     (positional < -1500)? FF3:
-                                     (positional < -500)? FF4:
-                                     (positional < 500)? FF5:
-                                     (positional < 1500)? FF6:
-                                     (positional < 2500)? FF7:
-                                     (positional < 3500)? FF8:FF9):
-                    (psqt < 2500)?  ((positional < -3500)? GG1:
-                                     (positional < -2500)? GG2:
-                                     (positional < -1500)? GG3:
-                                     (positional < -500)? GG4:
-                                     (positional < 500)? GG5:
-                                     (positional < 1500)? GG6:
-                                     (positional < 2500)? GG7:
-                                     (positional < 3500)? GG8:GG9):
-                     (psqt < 3500)?  ((positional < -3500)? HH1:
-                                     (positional < -2500)? HH2:
-                                     (positional < -1500)? HH3:
-                                     (positional < -500)? HH4:
-                                     (positional < 500)? HH5:
-                                     (positional < 1500)? HH6:
-                                     (positional < 2500)? HH7:
-                                     (positional < 3500)? HH8:HH9):
-                                    ((positional < -3500)? II1:
-                                     (positional < -2500)? II2:
-                                     (positional < -1500)? II3:
-                                     (positional < -500)? II4:
-                                     (positional < 500)? II5:
-                                     (positional < 1500)? II6:
-                                     (positional < 2500)? II7:
-                                     (positional < 3500)? II8:II9)) * positional;
+  Value scaled_positional = 1048 * positional;
 
     Value nnue = (scaled_psqt + scaled_positional) / 1024;
 
