5fa2afc6afcf0b16405e20d9f58f3ad044a34b4a
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-05 14:31:31 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1366,7 +1366,7 @@ moves_loop:  // When in check, search starts here
     // Adjust best value for fail high cases at non-pv nodes
     if (!PvNode && bestValue >= beta && !is_decisive(bestValue) && !is_decisive(beta)
         && !is_decisive(alpha)) {
-      if (depth <= 10)
+      if (depth <= 12)
         bestValue = (bestValue + beta) / 2;
       else
         bestValue = (bestValue * depth + beta) / (depth + 1);
