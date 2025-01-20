d30dcbdc70ee0ed88e9e36ce9dd74b35411c5565
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-20 16:00:45 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -809,7 +809,8 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 215, 7) + depth / 3 + 5;
+        Depth R = std::min(int(eval - beta) / 215, 7) + depth / 3 + 8
+         - std::min(std::abs(correctionValue) / 16777216, 3);
 
         ss->currentMove                   = Move::null();
         ss->continuationHistory           = &thisThread->continuationHistory[0][0][NO_PIECE][0];
