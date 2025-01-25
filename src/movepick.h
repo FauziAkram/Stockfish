409280788509b24945ae0c55b27d729d2b10eb63
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-26 02:35:21 +0300

    Update movepick.h

--- src/movepick.h
+++ src/movepick.h
@@ -46,7 +46,8 @@ class MovePicker {
                const CapturePieceToHistory*,
                const PieceToHistory**,
                const PawnHistory*,
-               int);
+               int,
+               bool qChecks = false);
     MovePicker(const Position&, Move, int, const CapturePieceToHistory*);
     Move next_move();
     void skip_quiet_moves();
