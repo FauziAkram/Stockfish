e45da1b69ea2fbf2a21b07916b0513f2f7a58679
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-26 02:16:21 +0300

    Update movepick.h

--- src/movepick.h
+++ src/movepick.h
@@ -72,6 +72,7 @@ class MovePicker {
     Depth                        depth;
     int                          ply;
     bool                         skipQuiets = false;
+    bool                         includeQChecks = false;
     ExtMove                      moves[MAX_MOVES];
 };
 
