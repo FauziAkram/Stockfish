a7330e9f5f0fac7cfd372c7c742276227b5c3f50
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-29 14:50:04 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture but the current move is not a capture
         if (ttCapture && !capture)
-            r += 1087 + (depth < 8 ? (1024 * (8 - depth) / 7) : 0)
+            r += 1087 + (depth < 8 ? (1024 * (8 - depth) / 7) : 0);
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 3)
