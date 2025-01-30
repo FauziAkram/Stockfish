02301b7065d3a59e8312f5c9e47cc2bc4bc19211
FauziAkram <fauzi.dabat@hotmail.com>
2025-01-30 20:46:58 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1415,6 +1415,10 @@ moves_loop:  // When in check, search starts here
     if (bestValue <= alpha)
         ss->ttPv = ss->ttPv || (ss - 1)->ttPv;
 
+    // Update for continuation history after fail-high at all nodes
+    if (!pos.capture_stage(bestMove) && bestValue >= beta)
+    update_continuation_histories(ss, movedPiece, bestMove.to_sq(), 657);
+
     // Write gathered information in transposition table. Note that the
     // static evaluation is saved as it was before correction history.
     if (!excludedMove && !(rootNode && thisThread->pvIdx))
