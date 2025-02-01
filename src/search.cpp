25dd965e5e36345da156731eafe4e3f9e7617dba
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-01 03:19:17 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1077,6 +1077,9 @@ moves_loop:  // When in check, search starts here
                 {
                     int corrValAdj   = std::abs(correctionValue) / 262144;
                     int doubleMargin = 249 * PvNode - 194 * !ttCapture - corrValAdj;
+                    dbg_mean_of(doubleMargin);
+dbg_extremes_of(doubleMargin);
+
                     int tripleMargin =
                       94 + 287 * PvNode - 249 * !ttCapture + 99 * ss->ttPv - corrValAdj;
                     int quadMargin =
