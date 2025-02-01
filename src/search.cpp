3e6373dfb6d677c1ad37524de7b29ff80f72960b
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-01 17:45:34 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -51,7 +51,8 @@
 #include "ucioption.h"
 
 namespace Stockfish {
-
+int xx1= 16, xx2=8;
+TUNE(SetRange(1, 50), xx1,xx2);
 namespace TB = Tablebases;
 
 void syzygy_extend_pv(const OptionsMap&            options,
@@ -1327,7 +1328,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += (extension < 2);
+                    ss->cutoffCnt += (depth < xx1) * (extension < 2) + (depth < xx2) * (extension < 1);
                     assert(value >= beta);  // Fail high
                     break;
                 }
