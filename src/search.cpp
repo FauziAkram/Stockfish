af76bf2c3a48a785c5a8ca11625f6b68396dd088
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-02 22:25:08 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1027,7 +1027,7 @@ moves_loop:  // When in check, search starts here
                 {
                     Value futilityValue = ss->staticEval + 271 + 243 * lmrDepth
                                         + PieceValue[capturedPiece] + captHist / 7;
-                    if (futilityValue <= alpha)
+                    if (futilityValue < beta)
                         continue;
                 }
 
