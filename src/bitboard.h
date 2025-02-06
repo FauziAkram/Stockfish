481c6f8ce0f075d8bcf5800e5fdd15831884a918
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-06 04:12:38 +0300

    Update bitboard.h

--- src/bitboard.h
+++ src/bitboard.h
@@ -149,12 +149,6 @@ constexpr Bitboard shift(Bitboard b) {
 
 // Returns the squares attacked by pawns of the given color
 // from the squares in the given bitboard.
-template<Color C>
-constexpr Bitboard pawn_attacks_bb(Bitboard b) {
-    return C == WHITE ? shift<NORTH_WEST>(b) | shift<NORTH_EAST>(b)
-                      : shift<SOUTH_WEST>(b) | shift<SOUTH_EAST>(b);
-}
-
 inline Bitboard pawn_attacks_bb(Color c, Square s) {
 
     assert(is_ok(s));
