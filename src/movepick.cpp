7f2cd5e83684301fc2930cf91abca8a846a8b63a
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-08 16:19:24 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -26,7 +26,8 @@
 #include "position.h"
 
 namespace Stockfish {
-
+int mv1=16384, mv2=51700, mv3=28600, mv4=14450, mv5=49000, mv6=24335, mv7=8, mv8=3560, mv9=7998;
+TUNE(mv1,mv2,mv3,mv4,mv5,mv6,mv7,mv8,mv9);
 namespace {
 
 enum Stages {
@@ -166,22 +167,22 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += bool(pos.check_squares(pt) & to) * 16384;
+            m.value += bool(pos.check_squares(pt) & to) * mv1;
 
             // bonus for escaping from capture
-            m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
-                                                  : pt == ROOK && !(to & threatenedByMinor) ? 25600
-                                                  : !(to & threatenedByPawn)                ? 14450
+            m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? mv2
+                                                  : pt == ROOK && !(to & threatenedByMinor) ? mv3
+                                                  : !(to & threatenedByPawn)                ? mv4
                                                                                             : 0)
                                                : 0;
 
             // malus for putting piece en prise
-            m.value -= (pt == QUEEN ? bool(to & threatenedByRook) * 49000
-                        : pt == ROOK && bool(to & threatenedByMinor) ? 24335
+            m.value -= (pt == QUEEN ? bool(to & threatenedByRook) * mv5
+                        : pt == ROOK && bool(to & threatenedByMinor) ? mv6
                                                                      : 0);
 
             if (ply < LOW_PLY_HISTORY_SIZE)
-                m.value += 8 * (*lowPlyHistory)[ply][m.from_to()] / (1 + 2 * ply);
+                m.value += mv7 * (*lowPlyHistory)[ply][m.from_to()] / (1 + 2 * ply);
         }
 
         else  // Type == EVASIONS
@@ -212,7 +213,7 @@ Move MovePicker::select(Pred filter) {
 // picking the move with the highest score from a list of generated moves.
 Move MovePicker::next_move() {
 
-    auto quiet_threshold = [](Depth d) { return -3560 * d; };
+    auto quiet_threshold = [](Depth d) { return -mv8 * d; };
 
 top:
     switch (stage)
@@ -263,7 +264,7 @@ top:
     case GOOD_QUIET :
         if (!skipQuiets && select([]() { return true; }))
         {
-            if ((cur - 1)->value > -7998 || (cur - 1)->value <= quiet_threshold(depth))
+            if ((cur - 1)->value > -mv9 || (cur - 1)->value <= quiet_threshold(depth))
                 return *(cur - 1);
 
             // Remaining quiets are bad
