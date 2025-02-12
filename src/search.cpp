956bfb0999fcd01308db93aaf758eac74073cb2b
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-12 13:45:05 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1055,7 +1055,7 @@ moves_loop:  // When in check, search starts here
 
                 int diff = ss->staticEval - bestValue;
 
-                Value futilityValue = ss->staticEval + std::clamp(diff - 50, 50, 135) + std::clamp(diff - 150, 150, 200) * lmrDepth;
+                Value futilityValue = ss->staticEval + std::clamp(diff - 50, 0, 135) + std::clamp(diff - 150, 150, 200) * lmrDepth;
 
                 // Futility pruning: parent node
                 if (!ss->inCheck && lmrDepth < 12 && futilityValue <= alpha)
