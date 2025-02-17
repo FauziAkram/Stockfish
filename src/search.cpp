613c0c33ef3205930a84dbf8dc1475448ca4cf91
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-17 21:30:16 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1566,7 +1566,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         if (bestValue >= beta)
         {
             if (!is_decisive(bestValue))
-                bestValue = (bestValue + beta) / 2;
+                bestValue = (46 * bestValue + 44 * beta) / 100;
             if (!ss->ttHit)
                 ttWriter.write(posKey, value_to_tt(bestValue, ss->ply), false, BOUND_LOWER,
                                DEPTH_UNSEARCHED, Move::none(), unadjustedStaticEval,
