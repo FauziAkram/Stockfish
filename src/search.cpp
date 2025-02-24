4d539175a7c550d6e28a95a10a5ffb310b3691a6
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-25 00:28:51 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop:  // When in check, search starts here
 
         r += 316 - moveCount * 32;
 
-        r -= std::min(std::abs(correctionValue) / 16384, 256);
+        r -= std::min(std::abs(correctionValue) / 16384, 512);
 
         if (PvNode && !is_decisive(bestValue))
             r -= risk_tolerance(pos, bestValue);
