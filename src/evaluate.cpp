1b2d800101a36522326172e8a199481d93812cd9
FauziAkram <fauzi.dabat@hotmail.com>
2025-02-28 01:06:47 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,6 +81,9 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     int material = 535 * pos.count<PAWN>() + pos.non_pawn_material();
     int v        = (nnue * (77777 + material) + optimism * (7777 + material)) / 77777;
 
+    // Evaluation grain (to get more alpha-beta cuts) with randomization (for robustness)
+    v = (v / 4) * 4 - 1 + (pos.key() & 0x2);
+  
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
 
