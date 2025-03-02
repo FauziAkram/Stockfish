62195fd3364e7ae795cb0b00670c4b5b31e3683e
FauziAkram <fauzi.dabat@hotmail.com>
2025-03-02 15:42:11 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1677,13 +1677,6 @@ dbg_extremes_of((*contHist[0])[pos.moved_piece(move)][move.to_sq()]
                        + (*contHist[1])[pos.moved_piece(move)][move.to_sq()]
                        + thisThread->pawnHistory[pawn_structure_index(pos)][pos.moved_piece(move)]
                                                 [move.to_sq()], 3);
-
-dbg_mean_of((*contHist[2])[pos.moved_piece(move)][move.to_sq()], 4);
-dbg_extremes_of((*contHist[2])[pos.moved_piece(move)][move.to_sq()], 4);
-dbg_mean_of((*contHist[3])[pos.moved_piece(move)][move.to_sq()], 5);
-dbg_extremes_of((*contHist[3])[pos.moved_piece(move)][move.to_sq()], 5); 
-dbg_mean_of((*contHist[4])[pos.moved_piece(move)][move.to_sq()], 6);
-dbg_extremes_of((*contHist[4])[pos.moved_piece(move)][move.to_sq()], 6);
             }
 
 
