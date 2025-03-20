3ac4f6846ee8c3f67a7f82bc66dfc9534d8c8ef7
FauziAkram <fauzi.dabat@hotmail.com>
2025-03-20 03:00:16 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1410,7 +1410,7 @@ moves_loop:  // When in check, search starts here
     // Adjust best value for fail high cases at non-pv nodes
     if (!PvNode && bestValue >= beta && !is_decisive(bestValue) && !is_decisive(beta)
         && !is_decisive(alpha))
-        bestValue = (bestValue * depth + beta) / (depth + 1);
+        bestValue = (bestValue * (depth + 1) + beta) / (depth + 2);
 
     if (!moveCount)
         bestValue = excludedMove ? alpha : ss->inCheck ? mated_in(ss->ply) : VALUE_DRAW;
