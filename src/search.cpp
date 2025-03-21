e9af9a243ebcb2ed0aff86f885d95f75699aab28
FauziAkram <fauzi.dabat@hotmail.com>
2025-03-21 19:48:16 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -50,7 +50,8 @@
 #include "ucioption.h"
 
 namespace Stockfish {
-
+int xx=110;
+TUNE(xx1);
 namespace TB = Tablebases;
 
 void syzygy_extend_pv(const OptionsMap&            options,
@@ -71,7 +72,7 @@ namespace {
 
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorsening) {
-    Value futilityMult       = 110 - 25 * noTtCutNode;
+    Value futilityMult       = xx1 - 25 * noTtCutNode;
     Value improvingDeduction = improving * futilityMult * 2;
     Value worseningDeduction = oppWorsening * futilityMult / 3;
 
