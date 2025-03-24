bda5ffeae8728ec288dc5e0d3e9c2627282c0a83
FauziAkram <fauzi.dabat@hotmail.com>
2025-03-24 17:02:24 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -76,6 +76,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
 
     // Blend optimism and eval with nnue complexity
     int nnueComplexity = std::abs(psqt - positional);
+if ((std::abs(optimism) * nnueComplexity) > 45000)
     optimism += optimism * nnueComplexity / 468;
     nnue -= nnue * nnueComplexity / 18000;
 
