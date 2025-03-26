855ff2a680068a634868b6eb83935a7e00ecab9a
FauziAkram <fauzi.dabat@hotmail.com>
2025-03-26 16:27:31 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -2021,8 +2021,8 @@ void SearchManager::check_time(Search::Worker& worker) {
       dbg_hit_on((worker.limits.movetime && elapsed >= worker.limits.movetime), 42);
       dbg_hit_on((worker.limits.nodes && worker.threads.nodes_searched() >= worker.limits.nodes), 43);
 
-        worker.threads.stop = worker.threads.abortedSearch = true;}
-}
+        worker.threads.stop = worker.threads.abortedSearch = true; }
+}}
 
 // Used to correct and extend PVs for moves that have a TB (but not a mate) score.
 // Keeps the search based PV for as long as it is verified to maintain the game
