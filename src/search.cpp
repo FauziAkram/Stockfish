847084429524d3e8dc4d904a89c4aea8167c56bb
FauziAkram <fauzi.dabat@hotmail.com>
2025-04-21 03:20:48 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -976,7 +976,7 @@ moves_loop:  // When in check, search starts here
     // Step 12. A small Probcut idea
     probCutBeta = beta + 415;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
-        && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
+        && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value) && !excludedMove)
         return probCutBeta;
 
     const PieceToHistory* contHist[] = {
