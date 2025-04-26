826ba6d5ea32416f46e1396b5c0475925c8dd424
FauziAkram <fauzi.dabat@hotmail.com>
2025-04-26 17:14:51 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1937,8 +1937,8 @@ void update_quiet_histories(
     Color us = pos.side_to_move();
     workerThread.mainHistory[us][move.from_to()] << bonus;  // Untuned to prevent duplicate effort
 
-    if (ss->ply < LOW_PLY_HISTORY_SIZE)
-        workerThread.lowPlyHistory[ss->ply][move.from_to()] << bonus * 829 / 1024;
+    if (ss->ply < 3)
+        workerThread.lowPlyHistory[ss->ply][move.from_to()] << bonus * 1024 / 1024;
 
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(), bonus * 1004 / 1024);
 
