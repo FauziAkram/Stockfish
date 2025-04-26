8833f26bc76780ee36c8318123eb24c20f913ca1
FauziAkram <fauzi.dabat@hotmail.com>
2025-04-27 02:59:38 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -189,14 +189,10 @@ void MovePicker::score() {
         }
 
         else  // Type == EVASIONS
-        {
-            if (pos.capture_stage(m))
-                m.value = PieceValue[pos.piece_on(m.to_sq())] + (1 << 28);
-            else
-                m.value = (*mainHistory)[pos.side_to_move()][m.from_to()]
+                m.value = PieceValue[pos.piece_on(m.to_sq())] + (1 << 28) * (pos.capture_stage(m))
+                        + (*mainHistory)[pos.side_to_move()][m.from_to()]
                         + (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
                         + (*pawnHistory)[pawn_structure_index(pos)][pos.moved_piece(m)][m.to_sq()];
-        }
 }
 
 // Returns the next move satisfying a predicate function.
