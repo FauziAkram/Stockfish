64608bf1fd9b4e71ba3386638c11b3cb8a4ea61f
FauziAkram <fauzi.dabat@hotmail.com>
2025-05-16 13:29:30 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -466,7 +466,9 @@ void Search::Worker::iterative_deepening() {
             fallingEval = std::clamp(fallingEval, 0.5786, 1.6752);
 
             // If the bestMove is stable over several iterations, reduce time accordingly
-            timeReduction = lastBestMoveDepth + 8 < completedDepth ? 1.4857 : 0.7046;
+            double k = 0.556;
+            double center = lastBestMoveDepth + 11;
+            timeReduction = 0.859 + 0.745 / (1.154 + std::exp(-k * (completedDepth - center)));
             double reduction =
               (1.4540 + mainThread->previousTimeReduction) / (2.1593 * timeReduction);
             double bestMoveInstability = 0.9929 + 1.8519 * totBestMoveChanges / threads.size();
