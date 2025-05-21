6578b4fac73af2a332ad48ee3a642881add59374
FauziAkram <fauzi.dabat@hotmail.com>
2025-05-21 14:45:41 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1661,7 +1661,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         moveCount++;
 
         // Step 6. Pruning
-        if (!is_loss(bestValue))
+        if (!ss->inCheck && !is_loss(bestValue))
         {
             // Futility pruning and moveCount pruning
             if (!givesCheck && move.to_sq() != prevSq && !is_loss(futilityBase)
