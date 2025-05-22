eeb4cab11bf16186b54d38d52e2f4d2ff8d853d7
FauziAkram <fauzi.dabat@hotmail.com>
2025-05-22 15:43:44 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1934,10 +1934,10 @@ void update_quiet_histories(
     workerThread.mainHistory[us][move.from_to()] << bonus;  // Untuned to prevent duplicate effort
 
     if (ss->ply < LOW_PLY_HISTORY_SIZE)
-        workerThread.lowPlyHistory[ss->ply][move.from_to()] << bonus * (bonus > -147 ? 855: 773) / 1024;
+        workerThread.lowPlyHistory[ss->ply][move.from_to()] << bonus * (bonus > -147 ? 855 : 773) / 1024;
 
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(),
-                                  bonus * (bonus > 7 ? 993 :702) / 1024);
+                                  bonus * (bonus > 7 ? 993 : 702) / 1024);
 
     int pIndex = pawn_structure_index(pos);
     workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()]
