e1024ff8e81ef43df2f3f6d5285e7bcb8f8a0d0f
FauziAkram <fauzi.dabat@hotmail.com>
2025-05-25 23:50:21 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop:  // When in check, search starts here
                                  - 997 * ttMoveHistory / 131072
                                  - (ss->ply > thisThread->rootDepth) * 47;
                 int tripleMargin = 84 + 269 * PvNode - 253 * !ttCapture + 91 * ss->ttPv
-                                 - corrValAdj2 - (ss->ply * 2 > thisThread->rootDepth * 3) * 54;
+                                 - corrValAdj2 - (ss->ply >= thisThread->rootDepth * 2) * 53;
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
