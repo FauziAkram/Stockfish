9925cce73fbec6c403d709f11339032da3eaf81b
FauziAkram <fauzi.dabat@hotmail.com>
2025-06-03 00:59:02 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -896,8 +896,8 @@ Value Search::Worker::search(
     // Step 10. Internal iterative reductions
     // For PV nodes without a ttMove as well as for deep enough cutNodes, we decrease depth.
     // (*Scaler) Especially if they make IIR less aggressive.
-    if (!allNode && depth >= 6 && !ttData.move)
-        depth--;
+    if (PvNode && depth >= 6 && !ttData.move)
+        depth -= cutNode;
 
     // Step 11. ProbCut
     // If we have a good enough capture (or queen promotion) and a reduced search
