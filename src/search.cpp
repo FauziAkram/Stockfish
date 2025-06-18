1881346d2b3a5685b4e5cc927e3917a7be61b355
FauziAkram <fauzi.dabat@hotmail.com>
2025-06-18 21:02:07 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1203,7 +1203,7 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r += 1210 + (depth < 8) * 963;
 
-        if ((ss + 2)->cutoffCnt >= 24)
+        if ((ss + 2)->cutoffCnt >= 24 && allNode)
             r += 1024;
 
         // Increase reduction if next ply has a lot of fail high
