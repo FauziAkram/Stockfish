b7d1e14f2a55c27f8d5c55eadee4ffed0529cbaa
FauziAkram <fauzi.dabat@hotmail.com>
2025-06-22 04:01:54 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -688,7 +688,8 @@ Value Search::Worker::search(
 
             // Extra penalty for early quiet moves of the previous ply
             if (prevSq != SQ_NONE && (ss - 1)->moveCount <= 3 && !priorCapture)
-                update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq, -2301);
+                update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
+                                         -std::min(809 * (depth + 1) - 249, 3000));
         }
 
         // Partial workaround for the graph history interaction problem
