9334fdcd9c335757c8bdc9c8f2d71ca1d74c5ca3
FauziAkram <fauzi.dabat@hotmail.com>
2025-06-25 18:50:47 +0300

    Update timeman.cpp

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -127,7 +127,7 @@ void TimeManagement::init(Search::LimitsType& limits,
     else
     {
         optScale =
-          std::min((0.88 + ply / 116.4) / (centiMTG / 100.0), 0.88 * limits.time[us] / timeLeft);
+          std::min(0.88 / (centiMTG / 100.0), 0.88 * limits.time[us] / timeLeft);
         maxScale = 1.3 + 0.11 * (centiMTG / 100.0);
     }
 
