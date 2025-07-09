7dcc82544066c69116f8279731d9985a12ca9721
FauziAkram <fauzi.dabat@hotmail.com>
2025-07-09 14:20:12 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1087,7 +1087,7 @@ moves_loop:  // When in check, search starts here
 
                 Value baseFutility = (bestMove ? 46 : 230);
                 Value futilityValue =
-                  ss->staticEval + baseFutility + 117 * lmrDepth + 102 * (ss->staticEval > alpha);
+                  ss->staticEval + baseFutility + 102 * (ss->staticEval > alpha);
 
                 // Futility pruning: parent node
                 // (*Scaler): Generally, more frequent futility pruning
