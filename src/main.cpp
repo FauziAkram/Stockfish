8e58a073da4d1f367f8d063d32102aa5410ab478
FauziAkram <fauzi.dabat@hotmail.com>
2025-07-15 23:38:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,14 @@
-/*
-  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2025 The Stockfish developers (see AUTHORS file)
-
-  Stockfish is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  Stockfish is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <iostream>
 
-#include "bitbases.h"
 #include "bitboard.h"
 #include "misc.h"
 #include "position.h"
 #include "types.h"
 #include "uci.h"
 #include "tune.h"
+#include "bitbases.h"
+#include "psqt.h"
+#include "endgame.h"
 
 using namespace Stockfish;
 
@@ -34,6 +18,9 @@ int main(int argc, char* argv[]) {
 
     Bitboards::init();
     Position::init();
+    PSQT::init();
+    Bitbases::init();
+    Endgames::init();
 
     UCIEngine uci(argc, argv);
 
