4ba325519aa0b00c001c41acaa8fb8eeebac3bcb
FauziAkram <fauzi.dabat@hotmail.com>
2025-08-25 01:26:22 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1888,7 +1888,7 @@ void update_quiet_histories(const Position& pos,
 
     int pIndex = pawn_history_index(pos);
     workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()]
-      << (bonus * (bonus > 0 ? 704 : 439) / 1024) + 70;
+      << (bonus * (bonus > 0 ? (PvNode? 704: 650) : 439) / 1024) + 70;
 }
 
 }
