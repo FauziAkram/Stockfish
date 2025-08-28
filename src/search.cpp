43e97c1326de9588dfc9c08937f9e898026c6e2b
FauziAkram <fauzi.dabat@hotmail.com>
2025-08-28 16:11:24 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1814,7 +1814,7 @@ void update_all_stats(const Position& pos,
     PieceType              capturedPiece;
 
     int bonus = std::min(151 * depth - 91, 1730) + 302 * (bestMove == ttMove);
-    int malus = std::min(951 * depth - 156, 2468) - 30 * quietsSearched.size();
+    int malus = 2468 - 30 * quietsSearched.size();
 
     if (!pos.capture_stage(bestMove))
     {
