39be756fcc7d35a3a3892a922ec39cb28183bbb2
FauziAkram <fauzi.dabat@hotmail.com>
2025-08-28 14:46:23 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1580,7 +1580,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         if (bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = ss->staticEval + 352;
+        futilityBase = std::max(ss->staticEval + 352, -3000);
     }
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
