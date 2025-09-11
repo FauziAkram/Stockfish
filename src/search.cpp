272b5a7c9287429185a99e0b6f1356425c0286f5
FauziAkram <fauzi.dabat@hotmail.com>
2025-09-11 04:28:10 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop:  // When in check, search starts here
             // singular (multiple moves fail high), and we can prune the whole
             // subtree by returning a softbound.
             else if (value >= beta && !is_decisive(value))
-                return std::max(beta, std::min(value, ttData.value));
+                return std::max(beta, std::min(value, (value + ttData.value) / 2));
 
             // Negative extensions
             // If other moves failed high over (ttValue - margin) without the
