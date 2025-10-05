8d1efdcf21a8f4f5fc971d3b3b20f7c702fbabfa
FauziAkram <fauzi.dabat@hotmail.com>
2025-10-05 16:55:08 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1135,7 +1135,7 @@ moves_loop:  // When in check, search starts here
             else if (value >= beta && !is_decisive(value))
             {
                 ttMoveHistory << std::max(-400 - 100 * depth, -4000);
-                return value;
+                return std::max(ttData.value, beta);
             }
 
             // Negative extensions
