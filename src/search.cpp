ffd9ca1a977a05d0e12dd55c9121069c7c4984e8
FauziAkram <fauzi.dabat@hotmail.com>
2025-10-20 20:12:47 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -50,6 +50,7 @@
 #include "ucioption.h"
 
 namespace Stockfish {
+int xx1=18, xx2=0, 	xx3=450, 	xx4=157, 	xx5=0, 	xx6=3900, 	xx7=4312, 	xx8=0, 	xx9=107800, 	xx10=63, 	xx11=0, 	xx12=508;
 
 namespace TB = Tablebases;
 
@@ -863,7 +864,7 @@ Value Search::Worker::search(
     }
 
     // Step 9. Null move search with verification search
-    if (cutNode && ss->staticEval >= beta - 18 * depth + 390 && !excludedMove
+    if (cutNode && ss->staticEval >= beta - std::min(xx1 * depth - xx2, xx3) + 390 && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta))
     {
         assert((ss - 1)->currentMove != Move::null());
@@ -1053,7 +1054,7 @@ moves_loop:  // When in check, search starts here
 
                 // SEE based pruning for captures and checks
                 // Avoid pruning sacrifices of our last piece for stalemate
-                int margin = std::max(157 * depth + captHist / 29, 0);
+                int margin = std::max(std::min(xx4 * depth - xx5, xx6) + captHist / 29, 0);
                 if ((alpha >= VALUE_DRAW || pos.non_pawn_material(us) != PieceValue[movedPiece])
                     && !pos.see_ge(move, -margin))
                     continue;
@@ -1065,7 +1066,7 @@ moves_loop:  // When in check, search starts here
                             + pawnHistory[pawn_history_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4312 * depth)
+                if (history < (-1 * std::min(xx7 * depth - xx8, xx9))
                     continue;
 
                 history += 76 * mainHistory[us][move.from_to()] / 32;
@@ -1401,7 +1402,7 @@ moves_loop:  // When in check, search starts here
     {
         int bonusScale = -228;
         bonusScale -= (ss - 1)->statScore / 104;
-        bonusScale += std::min(63 * depth, 508);
+        bonusScale += std::min(xx10 * depth - xx11, xx12);
         bonusScale += 184 * ((ss - 1)->moveCount > 8);
         bonusScale += 143 * (!ss->inCheck && bestValue <= ss->staticEval - 92);
         bonusScale += 149 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 70);
