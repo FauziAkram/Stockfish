53bc735d03f51a7c4f1bd4168d1058232538ba36
FauziAkram <fauzi.dabat@hotmail.com>
2026-01-27 14:05:12 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -877,7 +877,7 @@ Value Search::Worker::search(
     // The depth condition is important for mate finding.
     {
         auto futility_margin = [&](Depth d) {
-            Value futilityMult = (67+d) - (25-(d/4)) * !ss->ttHit;
+            Value futilityMult = 76 - (25-(d/4)) * !ss->ttHit;
 
             return futilityMult * d
                  - (2474 * improving + 331 * opponentWorsening) * futilityMult / 1024  //
