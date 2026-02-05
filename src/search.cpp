023af252032339ea5849dc77c1020776a72bf589
FauziAkram <fauzi.dabat@hotmail.com>
2026-02-05 19:20:42 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -755,7 +755,7 @@ Value Search::Worker::search(
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
     if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
-        depth--;
+        depth -= 1 + (priorReduction >= 3);
 
     // At non-PV nodes we check for an early TT cutoff
     if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
