f1e13d03e32332b7dd0458efd55c5566cf85e8dd
FauziAkram <fauzi.dabat@hotmail.com>
2026-02-15 14:19:55 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -777,7 +777,7 @@ Value Search::Worker::search(
         // For high rule50 counts don't produce transposition table cutoffs.
         if (pos.rule50_count() < 96)
         {
-            if (depth >= 8 && ttData.move && pos.pseudo_legal(ttData.move) && pos.legal(ttData.move)
+            if (ttData.move && pos.pseudo_legal(ttData.move) && pos.legal(ttData.move)
                 && !is_decisive(ttData.value))
             {
                 pos.do_move(ttData.move, st);
