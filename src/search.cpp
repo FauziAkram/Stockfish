8d9aa34b430cf0b6676b944a08d26efaae435406
FauziAkram <fauzi.dabat@hotmail.com>
2026-02-26 15:31:59 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1145,8 +1145,6 @@ moves_loop:  // When in check, search starts here
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
-
-                depth++;
             }
 
             // Multi-cut pruning
