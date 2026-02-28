b4da1ecf7c61fcb7a6a1ff3b63a9a07e40db5d1a
FauziAkram <fauzi.dabat@hotmail.com>
2026-02-28 14:44:41 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1209,8 +1209,8 @@ moves_loop:  // When in check, search starts here
             r += 249 + 1073 * ((ss + 1)->cutoffCnt > 2) + 1064 * allNode;
 
         // For first picked move (ttMove) reduce reduction
-        if (move == ttData.move)
-            r -= 2069;
+        else if (move == ttData.move)
+            r = std::max(0, r - 2048);
 
         if (capture)
             ss->statScore = 892 * int(PieceValue[pos.captured_piece()]) / 128
