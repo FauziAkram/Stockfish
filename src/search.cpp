25d69536ea505f20d47eb77eff120560d2e62aec
FauziAkram <fauzi.dabat@hotmail.com>
2026-04-17 16:26:36 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -942,12 +942,6 @@ Value Search::Worker::search(
 
     improving |= ss->staticEval >= beta;
 
-    // Step 10. Internal iterative reductions
-    // At sufficient depth, reduce depth for PV/Cut nodes without a TTMove.
-    // (*Scaler) Making IIR more aggressive scales poorly.
-    if (!ss->followPV && !allNode && !ttData.move && priorReduction <= 3)
-        r += std::clamp(interpolate(int(depth), 4, 8, 0, 1024), 0, 1024);
-
     // Step 11. ProbCut
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
@@ -1056,7 +1050,10 @@ moves_loop:  // When in check, search starts here
         int delta = beta - alpha;
 
         int r = reduction(improving, depth, moveCount, delta);
-
+      
+        if (!ss->followPV && !allNode && !ttData.move && priorReduction <= 3)
+            r += std::clamp(interpolate(int(depth), 4, 8, 0, 1024), 0, 1024);
+      
         // Increase reduction for ttPv nodes (*Scaler)
         // Larger values scale well
         if (ss->ttPv)
