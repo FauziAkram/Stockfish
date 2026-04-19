844bf799ee1c55fe19fc2fbe3de55ab6242c59c6
FauziAkram <fauzi.dabat@hotmail.com>
2026-04-19 18:24:01 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -345,7 +345,8 @@ bool Search::Worker::iterative_deepening() {
 
     for (Color c : {WHITE, BLACK})
         for (int i = 0; i < UINT_16_HISTORY_SIZE; i++)
-            mainHistory[c][i] = mainHistory[c][i] * xx30 / 1024;
+            mainHistory[c][i] = (mainHistory[c][i] - xx54) * xx30 / 1024;
+
 
     // Iterative deepening loop until requested to stop or the target depth is reached
     while (++rootDepth < MAX_PLY && !threads.stop
