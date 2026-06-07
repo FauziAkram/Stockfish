6cf592d7f849628d4655eb0a4de3517473f897f0
FauziAkram <fauzi.dabat@hotmail.com>
2026-06-07 15:22:31 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -813,7 +813,7 @@ Value Search::Worker::search(
         depth--;
 
     // At non-PV nodes we check for an early TT cutoff
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
+    if (!pseudoPvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
         && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || depth > 4))
