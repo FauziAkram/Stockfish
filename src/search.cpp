663454745639cb481cd470733f98a7f3b5a1faad
FauziAkram <fauzi.dabat@hotmail.com>
2026-06-20 00:48:39 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1346,9 +1346,7 @@ moves_loop:  // When in check, search starts here
                 const bool doDeeperSearch    = d < newDepth && value > bestValue + 52;
                 const bool doShallowerSearch = value < bestValue + 9;
 
-                newDepth += doDeeperSearch - doShallowerSearch;
-dbg_mean_of(r);
-dbg_extremes_of(r);
+                newDepth += doDeeperSearch - doShallowerSearch + (r<-5000);
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
