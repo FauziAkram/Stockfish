575378e7d1031d97cc387c25a4f2ce08e0bfd6bd
FauziAkram <fauzi.dabat@hotmail.com>
2026-06-23 17:13:54 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1360,8 +1360,8 @@ moves_loop:  // When in check, search starts here
         else if (!PvNode || moveCount > 1)
         {
             // Increase reduction if ttMove is not present
-            if (!ttData.move)
-                r += 1085;
+            if (!ttData.move || (ss + 1)->cutoffCnt > 3)
+                r += 1080;
 
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
