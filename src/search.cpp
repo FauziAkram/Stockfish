74e914a3dc63372c6f6faaf6056086388c501228
FauziAkram <fauzi.dabat@hotmail.com>
2026-07-01 14:01:26 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1309,7 +1309,7 @@ moves_loop:  // When in check, search starts here
             r += 236 + 1079 * ((ss + 1)->cutoffCnt > 2) + 1143 * allNode;
 
         // For first picked move (ttMove) reduce reduction
-        if (move == ttData.move)
+        else if (move == ttData.move)
             r -= 2016;
 
         if (capture)
