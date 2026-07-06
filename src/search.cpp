16739297861f866a09f3059e8899f5f8189967e3
FauziAkram <fauzi.dabat@hotmail.com>
2026-07-06 17:03:14 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1290,7 +1290,7 @@ moves_loop:  // When in check, search starts here
         // Decrease reduction for PvNodes (*Scaler)
         if (ss->ttPv)
             r -= 2766 + PvNode * 1017 + (ttData.value > alpha) * 838
-               + (ttData.depth >= depth) * (923 + cutNode * 955);
+               + (ttData.depth >= depth && cutNode) * 1738;
 
         r += 714;  // Base reduction offset to compensate for other tweaks
         r -= moveCount * 62;
