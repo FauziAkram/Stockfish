fee861b6d9525cee8a09a934a91130549b749d17
FauziAkram <fauzi.dabat@hotmail.com>
2026-07-17 02:48:09 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1274,7 +1274,7 @@ moves_loop:  // When in check, search starts here
 
             // If the ttMove is assumed to fail high over current beta
             else if (ttData.value >= beta)
-                extension = -3 - (ttData.value - beta > 3);
+                extension = -3 - (ttData.value - beta > 4);
 
             // If we are on a cutNode but the ttMove is not assumed to fail high
             // over current beta
