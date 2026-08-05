899d28d652971a96e1ee52e248ae70c1eb129adb
FauziAkram <fauzi.dabat@hotmail.com>
2026-08-05 14:21:21 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -321,7 +321,7 @@ top:
 
             endCur = endGenerated = score<QUIETS>(ml);
 
-            partial_insertion_sort(cur, endCur, -3560 * depth);
+            partial_insertion_sort(cur, endCur, -3218 * depth - 110);
         }
 
         ++stage;
