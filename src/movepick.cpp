b086b2e3bc52dba05dc8abfcd015a88083b28cea
FauziAkram <fauzi.dabat@hotmail.com>
2026-08-10 17:57:38 +0300

    Update movepick.cpp

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -231,7 +231,6 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
             m.value = 2 * (*mainHistory)[us][m.raw()];
             m.value += 2 * sharedHistory->pawn_entry(pos)[pc][to];
             m.value += (*continuationHistory[0])[pc][to];
-            m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to];
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
