b7a99d898fb90078584bdd552f61c59a1c8bab54
FauziAkram <fauzi.dabat@hotmail.com>
2026-09-01 02:15:27 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1352,7 +1352,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 439 / 4096;
 
         if (!capture && !is_decisive(alpha))
-            r += 3 * std::clamp(alpha - eval, -64, 96);
+            r += std::clamp(alpha - eval, -192, 288);
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
