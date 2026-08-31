85b2050f7a66b23eb2c3a2b4e08bc7ed4970b22e
FauziAkram <fauzi.dabat@hotmail.com>
2026-08-31 15:15:52 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1351,7 +1351,7 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 439 / 4096;
 
-        if (!capture && !is_decisive(alpha))
+        if (!capture && !is_decisive(alpha) && !givesCheck)
             r += 3 * std::clamp(alpha - eval, -64, 96);
 
         // Scale up reductions for expected ALL nodes
