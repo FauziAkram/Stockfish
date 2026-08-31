c3efc090c93c38ec34de3bfa0faea6fee126fe66
FauziAkram <fauzi.dabat@hotmail.com>
2026-08-31 20:37:00 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1248,6 +1248,14 @@ moves_loop:  // When in check, search starts here
             && ttData.depth >= depth - 3 && !is_shuffling(move, ss, pos) && !seekMate)
         {
             Value singularBeta  = ttData.value - (59 + 66 * (ss->ttPv && !PvNode)) * depth / 63;
+
+            if (rootMoves[0].averageScore != -VALUE_INFINITE) {
+                i64 avg = rootMoves[0].averageScore;
+                i64 meanSq = rootMoves[0].meanSquaredScore;
+                i64 rootVar = std::max(i64(0), meanSq - (avg * std::abs(avg)));
+                if (rootVar < 2300) singularBeta += 2;
+            }
+
             Depth singularDepth = newDepth / 2;
 
             ss->excludedMove = move;
