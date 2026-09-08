804593110056659c78e11aa841fe7a0300ccb135
FauziAkram <fauzi.dabat@hotmail.com>
2026-09-08 03:17:51 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1331,6 +1331,10 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r += 1079;
 
+        // Increase reduction on repetition
+        if (move == (ss - 4)->currentMove && pos.has_repeated())
+            r += 1024;
+
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 1)
             r += 264 + 1095 * ((ss + 1)->cutoffCnt > 2) + 1138 * allNode;
