82261bca32b0651acbd9f9d687ef9ee2ea6d5383
FauziAkram <fauzi.dabat@hotmail.com>
2026-09-10 16:17:53 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -872,7 +872,7 @@ Value Search::Worker::search(
 
     // Step 6. At non-PV nodes we check for an early TT cutoff. Note that we
     //         always check the validity of the TT value because of access races.
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
+    if (!PvNode && !excludedMove && ttData.depth > depth
         && is_valid(ttData.value)
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || depth > 4))
