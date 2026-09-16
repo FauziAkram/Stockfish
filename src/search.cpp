1ad1d9c6b414a3e8ce5078e077ebcb84695005f3
FauziAkram <fauzi.dabat@hotmail.com>
2026-09-16 16:33:26 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1350,6 +1350,9 @@ moves_loop:  // When in check, search starts here
         if (mp.stage == 2)
             r += 512;
 
+        else if (mp.stage == 9)
+            r -= 256;
+
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 1)
             r += 264 + 1095 * ((ss + 1)->cutoffCnt > 2) + 1138 * allNode;
