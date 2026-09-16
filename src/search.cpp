cd18dcd389414fb21661b1eaecaea5309d649605
FauziAkram <fauzi.dabat@hotmail.com>
2026-09-16 14:17:35 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1348,7 +1348,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for good captures
         if (mp.stage == 2)
-            r += 512;
+            r += 1024;
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 1)
